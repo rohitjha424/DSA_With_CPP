6dfdf28caf54e65fffcb258ea898d6e7d898d97e
rohitjha424 <rohitjha424@gmail.com>
2025-11-22 03:13:32 +0530

    BinToDecimal

---
+++ Number System/BinToDecimal.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int decimalToBinary(int num){
+int rem ;
+int bitToInt=0; //we need to print the result in int format
+int place=1;
+while(num>0){
+rem= num%2;
+bitToInt = rem * place + bitToInt;
+place= place*10;
+num=num/2;
+}
+return bitToInt;   
+}
+int binaryToDecimal(int binResult){
+    int binToDeci =0;
+    int place =1;
+    while(binResult){
+
+        int extractBit = binResult%10;
+        binToDeci = extractBit*place + binToDeci;
+        binResult= binResult/10;
+        place = place*2;
+    }
+    return binToDeci;
+}
+
+
+int main(){
+int num;
+cout<<"Enter a Decimal Number:"<<endl;
+cin>>num;
+int binResult= decimalToBinary(num);
+cout<<"Binary number is:"<<binResult<<endl;
+int binToDecimalRes= binaryToDecimal(binResult);
+cout<<"Decimal number is:"<<binToDecimalRes<<endl;
+}
