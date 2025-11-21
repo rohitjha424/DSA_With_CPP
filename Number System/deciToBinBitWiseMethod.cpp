68c9d082431792a5878bd0344603a9b30792aab0
rohitjha424 <rohitjha424@gmail.com>
2025-11-22 02:31:02 +0530

    Bitwise method

---
+++ Number System/deciToBinBitWiseMethod.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int decimaToBinaryMedthod(int num){
+int rem ;
+int bitToInt=0; //we need to print the result in int format
+int place=1;
+while(num>0){
+int bit = (num & 1);
+bitToInt = bit * place + bitToInt;
+place= place*10;
+num= num>>1;
+}
+return bitToInt;   
+}
+
+int main(){
+int num;
+cout<<"Enter a Decimal Number:"<<endl;
+cin>>num;
+int result= decimaToBinaryMedthod(num);
+cout<<"Binary number is:"<<result<<endl;
+}
