3192f4e8b1742144a5afe11d48d242d2775decb1
rohitjha424 <rohitjha424@gmail.com>
2025-11-27 00:12:16 +0530

    fixed Q3

--- Conditions And loops/DebugExcersiceTest/Q3.cpp
+++ Conditions And loops/DebugExcersiceTest/Q3.cpp
@@ -1,27 +1,36 @@
-#include <iostream>
+//Debug the code. It is trying to print the given pattern.
+
+/*
+Pattern
+N = 4
+   *
+  ***
+ *****
+*******
+*/
+#include<iostream>
 using namespace std;
-
-// Print a N Solid square pattern of Stars using foor loop as below.
-
-// * * * *
-// * * * *
-// * * * *
-// * * * *
-
-int main()
-{
-
+int main(){
     int n;
-    cout<<"Enter the number of N Square"<<endl;
     cin>>n;
-    for (int row = 0; row < n; row++)
-    {
-
-        for (int col = 0; col < n; col++)
-        {
-            cout << "* ";
+    int i=1;
+    while(i<=n){
+        int j = i,count = 2*i-1,gaps=(n-i),k=1;
+        while(k<=gaps){
+            cout<<" ";
+            k = k + 1;
         }
-        cout << endl;
+        int m = 1,ch = count;
+        while(m<=ch){
+            cout<<"*";
+            m = m + 1;
+        }
+        k = 1;
+        while(k<=gaps){
+            cout<<" ";
+            k = k + 1;
+        }
+        cout<<"\n";
+        i = i + 1;
     }
-    return 0;
 }
