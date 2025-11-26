c88a1dea2445bbb22dd3495fe9dbc5c6cbb4d47d
rohitjha424 <rohitjha424@gmail.com>
2025-11-27 00:42:11 +0530

    Q6 Fixed

---
+++ Conditions And loops/DebugExcersiceTest/Q6.cpp
@@ -0,0 +1,25 @@
+//6. Debug the code. It is trying to print the given pattern.
+/*
+Pattern
+N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i,j,n;;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        char p='A'-i+n;
+        for(j=1;j<=i;j++){
+            cout<<p;
+            p++;  
+        }
+        cout<<endl;
+    }
+}
