6f08f0a58677c48c7a4aafbfb17f3aa44cd5c948
rohitjha424 <rohitjha424@gmail.com>
2025-12-06 02:07:06 +0530

    Pattern4Type2 Solved

---
+++ Conditions And loops/AssignmentWeek1/pattern4Type2.cpp
@@ -0,0 +1,56 @@
+// Hollow Full Pyramid
+//         *
+//       *   *
+//     *       *
+//   *           *
+// * * * * * * * * *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int i = 1;
+    int j = 2 * n - 1;
+
+    while (i <= n)
+    {
+        int gap = n - i;
+        int count = 0;
+        int k = 1;
+        while (k <= gap)
+        {
+            cout << "  ";
+            k = k + 1;
+        }
+
+        count = 2 * i - 1;
+        k = 1;
+        while (k <= count)
+        {
+            if (k == 1 || k == count || i == n)
+            {
+
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+            k = k + 1;
+        }
+        k = 1;
+        while (k <= gap)
+        {
+            cout << "  ";
+            k = k + 1;
+        }
+        i = i + 1;
+        cout << endl;
+    }
+
+    return 0;
+}
