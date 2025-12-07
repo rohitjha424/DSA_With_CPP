bee451c435c15fac09df04dc7d35f927d8de2fac
rohitjha424 <rohitjha424@gmail.com>
2025-12-07 19:52:42 +0530

    FCQ 6 Solved

---
+++ Conditions And loops/AssignmentWeek1/FC6CheckPrime.cpp
@@ -0,0 +1,26 @@
+// Check prime or not
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int N;
+    cin >> N;
+    int i = 2;
+    while (i < N)
+    {
+        if (N % i == 0)
+        {
+            cout << "\n Not a Prime Number, divisible by " <<i<< endl;
+            return 0;
+        }
+        else
+        {
+            i = i + 1;
+        }
+    }
+    cout << "Prime Number";
+    return 0;
+}
