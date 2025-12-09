7902e9743cae7edbf89099d42342746bdc529e91
rohitjha424 <rohitjha424@gmail.com>
2025-12-09 23:58:54 +0530

    Solving Pyramid With For Loop

--- 2Basics of Programming - Level 2/PatternContinues/InvertedFullPyramidForLoop.cpp
+++ 2Basics of Programming - Level 2/PatternContinues/InvertedFullPyramidForLoop.cpp
@@ -9,33 +9,6 @@ int main()
     int n;
     cin >> n;
 
-    int i = n;
-    while (i >= 1)
-    {
-
-        int gaps = n - i;
-        int count = i;
-        int k = 1;
-        while (k <= gaps)
-        {
-            cout << " ";
-            k++;
-        }
-        k = 1;
-        while (k <= count)
-        {
-            cout << "* ";
-            k++;
-        }
-        k = 1;
-        while (k <= gaps)
-        {
-            cout << " ";
-            k++;
-        }
-        cout << endl;
-        i--;
-    }
 
     return 0;
 }
