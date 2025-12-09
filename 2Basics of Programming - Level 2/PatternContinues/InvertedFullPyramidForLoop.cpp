448156ce510da212999d348feac6a16f88794bba
rohitjha424 <rohitjha424@gmail.com>
2025-12-10 00:00:02 +0530

    Done

--- 2Basics of Programming - Level 2/PatternContinues/InvertedFullPyramidForLoop.cpp
+++ 2Basics of Programming - Level 2/PatternContinues/InvertedFullPyramidForLoop.cpp
@@ -3,12 +3,17 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-
+int main() {
     int n;
+    cout << "Enter number of rows: ";
     cin >> n;
 
+    for (int i = n; i >= 1; i--) {          // row loop
+        for (int j = 1; j <= i; j++) {      // stars loop
+            cout << "* ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
