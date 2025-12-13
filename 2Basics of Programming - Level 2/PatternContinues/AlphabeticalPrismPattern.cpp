4422e18f202f52c43df0b71200f794f09c72084a
rohitjha424 <rohitjha424@gmail.com>
2025-12-14 00:53:47 +0530

    Pattern solved

---
+++ 2Basics of Programming - Level 2/PatternContinues/AlphabeticalPrismPattern.cpp
@@ -0,0 +1,34 @@
+// Print below Pattern:
+// A
+// ABA
+// ABCBA
+// ABCDCBA
+// ABCDEDCBA
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    for (int row = 0; row < n; row++)
+    {
+        char ch;
+        for (int col = 0; col < 2 * row + 1; col++)
+        {
+            if (col <= row)
+            {
+                ch = col + 'A';
+                cout << ch;
+            }
+            else
+            {
+                ch = ch - 1;
+                cout << ch;
+            }
+        }
+        cout << endl;
+    }
+}
