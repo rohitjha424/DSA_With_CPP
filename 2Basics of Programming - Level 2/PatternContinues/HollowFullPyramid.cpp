dd9a45642d7b4cac76dffa6c254c9c69322ad144
rohitjha424 <rohitjha424@gmail.com>
2025-12-13 17:15:55 +0530

    Finally Solved Hollow Pyrmid Pattern without base

--- 2Basics of Programming - Level 2/PatternContinues/HollowFullPyramid.cpp
+++ 2Basics of Programming - Level 2/PatternContinues/HollowFullPyramid.cpp
@@ -1,26 +1,39 @@
-//Hollow full Pyramid without base
+// Hollow full Pyramid without base
+//     *
+//    * *
+//   *   *
+//  *     *
+// *       *
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
 
-    for(int row =0 ; row<n; row++){
+    for (int row = 0; row < n; row++)
+    {
 
-        for(int col=0; col<n-row+1; col++){
-            cout<<" ";
+        for (int col = 0; col < n - row - 1; col++)
+        {
+            cout << " ";
         }
-        for(int col=0; col<row+1; col++){
-            if(col==0 || col==row){
+        for (int col = 0; col < row + 1; col++)
+        {
+            if (col == 0 || col == row + 1 - 1)
+            {
 
-                cout<<"* ";
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
             }
         }
-            cout<<endl;
-
+        cout << endl;
     }
     return 0;
 }
