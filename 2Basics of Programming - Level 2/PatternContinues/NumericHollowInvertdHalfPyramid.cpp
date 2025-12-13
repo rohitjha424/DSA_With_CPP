0ddef4d6f79a961e039f7fa7b5fc5b0e6c14523d
rohitjha424 <rohitjha424@gmail.com>
2025-12-14 02:09:46 +0530

    Pattern Solved

--- 2Basics of Programming - Level 2/PatternContinues/NumericHollowInvertdHalfPyramid.cpp
+++ 2Basics of Programming - Level 2/PatternContinues/NumericHollowInvertdHalfPyramid.cpp
@@ -1,26 +1,36 @@
-//Print the below Numerical Hollow inverted half Pyramid.
+// Print the below Numerical Hollow inverted half Pyramid.
+// 1 2 3 4 5
+// 2     5
+// 3   5
+// 4 5
+// 5
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
 
-    for(int row =0; row <n; row++){
-        int count =row+1;
-        for(int col=0; col<n-row; col++){
-            if(col==0 || col == n-row-1){
+    for (int row = 0; row < n; row++)
+    {
+        int count = row + 1;
+        for (int col = 0; col < n - row; col++)
+        {
+            if (row == 0 || col == 0 || col == n - row - 1)
+            {
 
-                cout<<count;
+                cout << count << " ";
             }
-            else{
-                cout<<" ";
+            else
+            {
+                cout << "  ";
             }
             count++;
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
