46b97f75adc64dbc35c11ec2ae4bde1b726116a9
rohitjha424 <rohitjha424@gmail.com>
2025-12-14 17:49:12 +0530

    Bitwise AND Examples Added

---
+++ 2Basics of Programming - Level 2/BitwiseOperator/bitwiseANDOperator.cpp
@@ -0,0 +1,42 @@
+//Bitwise operatos : &, | , ~, ^
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a =2;
+    int b =3;
+
+    cout <<(a&b)<<endl;
+
+    /*  The will give output as 2.
+    Explanatins:
+    as this is biwtwise AND operator , it will perform operations on binary Equivalent of these numbers bitwise.
+    Binary Equivalenbt of :
+    2: 10
+    3: 11
+    -----
+       10 >>>>> 1&1 >>1 ;;;;;; 0&1 >>0 >>>>>10>>> int Quivaluent of 10 >>2. 
+       ----
+    
+    */
+
+    int x =5;
+    int y =10;
+
+    cout <<(x&y)<<endl;
+/*  The will give output as 0.
+    Explanatins:
+    as this is biwtwise AND operator , it will perform operations on binary Equivalent of these numbers bitwise.
+    Binary Equivalenbt of :
+    5: 00000101
+   10: 00001010
+    -----
+        00000000 >>>> Binrary Equivalent >> 0 
+       ----
+    
+    */
+
+    return 0;
+}
--- 2Basics of Programming - Level 2/BitwiseOperator/pro.cpp
+++
@@ -1,1 +0,0 @@
-//Bitwise operatos truth Table
