dae9bc48e1bbfbe1a8f5671389b098b4114d8be1
rohitjha424 <rohitjha424@gmail.com>
2025-12-18 01:24:54 +0530

    TypeCasting

--- 2Basics of Programming - Level 2/TypeCasting/typecasting.cpp
+++ 2Basics of Programming - Level 2/TypeCasting/typecasting.cpp
@@ -1,1 +1,15 @@
-//typecasting
+// typecasting
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num1 = 10;
+    float num2 = 5.5;
+    float result = num1 + num2; // int is added to float>>float>>high precison.
+    cout << result << endl;     // Will result in 15.5
+
+    return 0;
+}
