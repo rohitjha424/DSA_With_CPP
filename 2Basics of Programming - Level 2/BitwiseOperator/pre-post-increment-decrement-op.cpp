5d9a6ac4c6f45dfd481aea13e4f19df897879e7e
rohitjha424 <rohitjha424@gmail.com>
2025-12-17 23:57:04 +0530

    pre post inc decre operators

---
+++ 2Basics of Programming - Level 2/BitwiseOperator/pre-post-increment-decrement-op.cpp
@@ -0,0 +1,16 @@
+//Pre Post increment Decrement Operators.
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a =5;
+    
+    //Pre ++
+    ++a; 
+    cout<<a;// will print 6. 
+
+    
+
+    return 0;
+}
