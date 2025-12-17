a1026e0c81a25a6252f1dfcabe4078c40824bddf
rohitjha424 <rohitjha424@gmail.com>
2025-12-18 01:35:38 +0530

    char to int

--- 2Basics of Programming - Level 2/TypeCasting/implicitTypeCasting.cpp
+++ 2Basics of Programming - Level 2/TypeCasting/implicitTypeCasting.cpp
@@ -6,10 +6,21 @@ using namespace std;
 int main()
 {
 
+    // int to float
     int num1 = 10;
     float num2 = 5.5;
     float result = num1 + num2; // int is added to float>>float>>high precison.
     cout << result << endl;     // Will result in 15.5
+
+    // char to int
+    char ch = 'A';
+    int a = ch + 1;
+    /*
+    As ch ='A'>>Stored as 65 as ASCII int value>>
+    when added to int, it will implictly converted into an int.
+    >>result =66
+    */
+    cout << a << endl;
 
     return 0;
 }
