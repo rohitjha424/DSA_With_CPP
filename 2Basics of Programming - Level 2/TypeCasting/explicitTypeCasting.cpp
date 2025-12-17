cabd6883e9cab43c0189a92eb7d60c08df02f6df
rohitjha424 <rohitjha424@gmail.com>
2025-12-18 02:02:39 +0530

    explicit Typecasting

---
+++ 2Basics of Programming - Level 2/TypeCasting/explicitTypeCasting.cpp
@@ -0,0 +1,18 @@
+// Explicit TypeCasting
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // float to int
+    int num1 = 10;
+    float num2 = 5.5;
+    float result = num1 + (int)num2;
+    // num2 converted explictly into int.
+    // num1+num2>>10+5=15>> as result is float so 15.0
+    cout << result << endl; // Will result in 15.0
+
+    return 0;
+}
