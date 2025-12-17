0513e3f086a3ea7d73d13886c5398857cb672684
rohitjha424 <rohitjha424@gmail.com>
2025-12-18 02:04:36 +0530

    double to int

--- 2Basics of Programming - Level 2/TypeCasting/explicitTypeCasting.cpp
+++ 2Basics of Programming - Level 2/TypeCasting/explicitTypeCasting.cpp
@@ -9,10 +9,20 @@ int main()
     // float to int
     int num1 = 10;
     float num2 = 5.5;
-    float result = num1 + (int)num2;
+    float result = num1 + (int)num2; 
     // num2 converted explictly into int.
     // num1+num2>>10+5=15>> as result is float so 15.0
-    cout << result << endl; // Will result in 15.0
+    cout << result << endl;     // Will result in 15.0
+
+    //dount to int
+
+    double pi =  3.14159269;
+    int intPi= (int)pi; // convert the double to int, truncate the decimle part/
+    cout<<intPi<<endl; // print 3
+
+
+
+
 
     return 0;
 }
