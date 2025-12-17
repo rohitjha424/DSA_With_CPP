16f0ccb9f504a0a0f9ad13cf8930546bced99ed5
rohitjha424 <rohitjha424@gmail.com>
2025-12-18 00:06:29 +0530

    pre -- Added

--- 2Basics of Programming - Level 2/BitwiseOperator/pre-post-increment-decrement-op.cpp
+++ 2Basics of Programming - Level 2/BitwiseOperator/pre-post-increment-decrement-op.cpp
@@ -1,16 +1,20 @@
-//Pre Post increment Decrement Operators.
+// Pre Post increment Decrement Operators.
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int a =5;
-    
-    //Pre ++
-    ++a; 
-    cout<<a;// will print 6. 
+    int a = 5;
 
-    
+    // Pre ++
+    ++a;
+    cout << a << endl; // will print 6.
+
+    // Pre --
+    int b = 10;
+    --b;
+    cout << b << endl; // will print 9.
 
     return 0;
 }
