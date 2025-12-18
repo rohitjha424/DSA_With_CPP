2ba8a80c3aff4adcfc45b44153b51338b405cf40
rohitjha424 <rohitjha424@gmail.com>
2025-12-19 01:19:02 +0530

    Functions Example Added

---
+++ 2Basics of Programming - Level 2/Function in CPP/codeReadabilityExample.cpp
@@ -0,0 +1,24 @@
+// Function Example for code readability
+
+#include <iostream>
+using namespace std;
+
+void printName(string name){
+
+    for(int i =0 ; i<3; i++){
+        cout<<name<<endl;
+    }
+
+}
+
+
+int main(){
+
+    printName("Rohit");
+    printName("Sharma");
+    printName("Pooja");
+    printName("Kohli");
+    printName("Nitin");
+
+    return 0;
+}
