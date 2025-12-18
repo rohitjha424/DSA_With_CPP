55d187194cbf72e2f42fa0bfb829f93bd666d433
rohitjha424 <rohitjha424@gmail.com>
2025-12-19 01:11:17 +0530

    Formatting

--- 2Basics of Programming - Level 2/SwichCase/calculatorUsingSwitch.cpp
+++ 2Basics of Programming - Level 2/SwichCase/calculatorUsingSwitch.cpp
@@ -3,40 +3,42 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     float a, b, result;
-    cout<<" enter Two Numbers:"<<endl;
-    cin>>a>>b;
+    cout << " enter Two Numbers:" << endl;
+    cin >> a >> b;
     int op;
-    cout<<"What operation you wannna do?"<<endl;
-    cin>>op;
+    cout << "What operation you wannna do?" << endl;
+    cin >> op;
     // op= 0>>add ; op = 1>>sub; op = 2 >>multiply; op = 3>>divide;
 
-    switch(op){
-        case 0: 
-        cout<<"Addition of given Numbers is:";
-        result = a+b; 
+    switch (op)
+    {
+    case 0:
+        cout << "Addition of given Numbers is:";
+        result = a + b;
         break;
 
-        case 1: 
-        cout<<"Substraction of given Numbers is:";
-        result = a-b; 
+    case 1:
+        cout << "Substraction of given Numbers is:";
+        result = a - b;
         break;
 
-        case 2: 
-        cout<<"multiplication of given Numbers is:";
-        result = a*b; 
+    case 2:
+        cout << "multiplication of given Numbers is:";
+        result = a * b;
         break;
 
-        case 3: 
-        cout<<"Division of given Numbers is:";
-        result = a/b; 
+    case 3:
+        cout << "Division of given Numbers is:";
+        result = a / b;
         break;
-        default: cout<<"invalid operation"<<endl;
+    default:
+        cout << "invalid operation" << endl;
         break;
-
     }
-    cout<<result<<endl;
+    cout << result << endl;
     return 0;
 }
