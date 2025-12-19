f5802ed72d2bc0555db536db11acbaf2df648226
rohitjha424 <rohitjha424@gmail.com>
2025-12-19 16:52:47 +0530

    New Logic Added

--- 2Basics of Programming - Level 2/Function in CPP/sumOfAllNumber1ToN.cpp
+++ 2Basics of Programming - Level 2/Function in CPP/sumOfAllNumber1ToN.cpp
@@ -6,8 +6,14 @@ using namespace std;
 void sumN(int num)
 {
 
-    int result = (num*(num+1))/2;
-    cout<<result;
+    // int sum = (num*(num+1))/2;
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+
+    cout << sum;
 }
 
 int main()
