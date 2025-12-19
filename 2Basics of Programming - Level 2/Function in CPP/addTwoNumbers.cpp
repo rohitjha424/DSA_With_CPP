0198722645c261339c0d95a714cdbb1e5222e983
rohitjha424 <rohitjha424@gmail.com>
2025-12-19 10:59:44 +0530

    functions example added

---
+++ 2Basics of Programming - Level 2/Function in CPP/addTwoNumbers.cpp
@@ -0,0 +1,21 @@
+// Function to add two numbers
+
+#include <iostream>
+using namespace std;
+
+void addTwoNumbers(int a, int b)
+{
+
+    cout << "The Sum is: " << a + b << endl;
+}
+
+int main()
+{
+    int a, b;
+    cout << "enter tWo numbers:" << endl;
+    cin >> a >> b;
+
+    addTwoNumbers(a, b);
+
+    return 0;
+}
