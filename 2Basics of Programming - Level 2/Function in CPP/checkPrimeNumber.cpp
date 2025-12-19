60dcccf10f3d93f68915dc0d5884f27c8aa6d3de
rohitjha424 <rohitjha424@gmail.com>
2025-12-19 12:23:27 +0530

    PrimeNumberUsingFunctions

---
+++ 2Basics of Programming - Level 2/Function in CPP/checkPrimeNumber.cpp
@@ -0,0 +1,39 @@
+// Function to ceck prime Number.
+
+#include <iostream>
+using namespace std;
+
+void checkPrime(int num)
+{
+
+    if (num < 2)
+    {
+        cout << "Not a Prime Number !!! " << endl;
+    }
+    else if (num == 2)
+    {
+        cout << "Prime Number !!!" << endl;
+    }
+    else
+    {
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                cout << "Not a Prime Number !!!" << endl;
+                return;
+            }
+        }
+        cout << "Prime Number" << endl;
+    }
+}
+
+int main()
+{
+    int num;
+    cout << "enter a number:" << endl;
+    cin >> num;
+    checkPrime(num);
+
+    return 0;
+}
