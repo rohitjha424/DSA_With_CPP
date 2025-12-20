6139a2bb1d54e992115212884361353bb934e1d4
rohitjha424 <rohitjha424@gmail.com>
2025-12-20 18:19:47 +0530

    Array Started

---
+++ 3Arrays Level -1/ArraysIntro.cpp
@@ -0,0 +1,18 @@
+//Creation of Arrays
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5];
+    char ch[26];
+    bool flags[20];
+    long num[800];
+    short snum[100];
+    
+
+    cout<<"Array Created Successfully"<<endl;
+
+    return 0;
+}
