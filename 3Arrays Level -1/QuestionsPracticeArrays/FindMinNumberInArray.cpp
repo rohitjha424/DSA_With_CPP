c66dc61a9e362f23386e175a600c8ba0d0ac190b
rohitjha424 <rohitjha424@gmail.com>
2025-12-23 02:00:34 +0530

    FindMin in an Array Method 2

--- 3Arrays Level -1/QuestionsPracticeArrays/FindMinNumberInArray.cpp
+++ 3Arrays Level -1/QuestionsPracticeArrays/FindMinNumberInArray.cpp
@@ -5,32 +5,32 @@
 using namespace std;
 
 // Mthod 1 : Best Practice to use INT_MAX
- void findMin(int arr[], int size)
- {
-     int minNum = INT_MAX;
-     for (int i = 0; i < size; i++)
-     {
-         if (arr[i] < minNum)
-         {
-             minNum = arr[i];
-         }
-     }
-     cout << "Minimum Number in the given Array is: " << minNum << endl;
- }
+//  void findMin(int arr[], int size)
+//  {
+//      int minNum = INT_MAX;
+//      for (int i = 0; i < size; i++)
+//      {
+//          if (arr[i] < minNum)
+//          {
+//              minNum = arr[i];
+//          }
+//      }
+//      cout << "Minimum Number in the given Array is: " << minNum << endl;
+//  }
 
 // Method 2: Initialize minMun with first Array element and then compare further
-// void findMin(int arr[], int size)
-// {
-//     int minNum = arr[0];
-//     for (int i = 1; i < size; i++)
-//     {
-//         if (arr[i] < minNum)
-//         {
-//             minNum = arr[i];
-//         }
-//     }
-//     cout << "Minimum Number in the given Array is: " << minNum << endl;
-// }
+void findMin(int arr[], int size)
+{
+    int minNum = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] < minNum)
+        {
+            minNum = arr[i];
+        }
+    }
+    cout << "Minimum Number in the given Array is: " << minNum << endl;
+}
 
 // Mthod 3 : using inbuilt Function min()
 // void findMin(int arr[], int size)
