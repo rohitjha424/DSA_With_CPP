aaad030630448649714f8545df026fe4902df654
rohitjha424 <rohitjha424@gmail.com>
2025-12-22 23:14:40 +0530

    Array Intro

--- 3Arrays Level -1/ArraysIntro.cpp
+++ 3Arrays Level -1/ArraysIntro.cpp
@@ -1,18 +1,21 @@
-//Creation of Arrays
+// Arrays Intro
 
 #include <iostream>
 using namespace std;
 
 int main(){
 
-    int arr[5];
-    char ch[26];
-    bool flags[20];
-    long num[800];
-    short snum[100];
+    //Creation of Arrays
+    // int arr[5];
+    // char ch[26];
+    // bool flags[20];
+    // long num[800];
+    // short snum[100];
     
 
-    cout<<"Array Created Successfully"<<endl;
+    // cout<<"Array Created Successfully"<<endl;
+
+    int arr[100];
 
     return 0;
 }
