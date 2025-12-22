7a8cf79fa74569ccc9ee7fde9e41fffe6bea44b6
rohitjha424 <rohitjha424@gmail.com>
2025-12-22 23:25:03 +0530

    formatting

--- 3Arrays Level -1/ArraysIntro.cpp
+++ 3Arrays Level -1/ArraysIntro.cpp
@@ -3,26 +3,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    //Creation of Arrays
-    // int arr[5];
-    // char ch[26];
-    // bool flags[20];
-    // long num[800];
-    // short snum[100];
-    
+    // Creation of Arrays
+    //  int arr[5];
+    //  char ch[26];
+    //  bool flags[20];
+    //  long num[800];
+    //  short snum[100];
 
     // cout<<"Array Created Successfully"<<endl;
 
     // int arr[100];
-    int a=5;
-    cout<<"Adreess of a is : "<<&a<<endl;
-    
-    int arr[10];
-    cout<<"Base adreess of arr is : "<<&arr<<endl;
+    int a = 5;
+    cout << "Adreess of a is : " << &a << endl;
 
-    
+    int arr[10];
+    cout << "Base adreess of arr is : " << &arr << endl;
 
     return 0;
 }
