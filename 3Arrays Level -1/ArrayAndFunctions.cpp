9dc18041589b713522765ac8a95bf89f32ff3ee5
rohitjha424 <rohitjha424@gmail.com>
2025-12-23 00:56:04 +0530

    linear Search using Array and functions

--- 3Arrays Level -1/ArrayAndFunctions.cpp
+++ 3Arrays Level -1/ArrayAndFunctions.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 using namespace std;
 
+// Print Array
 void printArray(int arr[], int size)
 {
     for (int i = 0; i < size; i++)
@@ -12,10 +13,36 @@ void printArray(int arr[], int size)
     cout << endl;
 }
 
+// Linear Search
+bool lineaSearch(int arr[], int size, int target)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+
 int main()
 {
     int arr[5] = {2, 4, 6, 8, 10};
     int size = 5;
     printArray(arr, size);
+
+    int target = 6;
+    bool ifFound = lineaSearch(arr, size, target);
+    if (ifFound)
+    {
+        cout << "Target Element: " << target << " found" << endl;
+    }
+    else
+    {
+        cout << "Element not found " << endl;
+    }
     return 0;
 }
