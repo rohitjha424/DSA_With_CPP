51e8b1381b4f37f2ec190d91bf5ece52e13ea6da
rohitjha424 <rohitjha424@gmail.com>
2025-12-23 00:20:52 +0530

    logic changed

--- 3Arrays Level -1/LinearSearchInArray.cpp
+++ 3Arrays Level -1/LinearSearchInArray.cpp
@@ -5,18 +5,30 @@ using namespace std;
 
 int main()
 {
-    
-    int arr[] = {2,4,6,8,10,12};
-    int n =6;
+
+    int arr[] = {2, 4, 6, 8, 10, 12};
+    int n = 6;
     // find the value 10
     int target = 10;
-    for (int i = 0; i < n; i++)
+
+    int i;
+    bool ifFound = false;
+    for (i = 0; i < n; i++)
     {
-        if(arr[i]==target){
-            cout << "Element found at index: "<<i<< endl;
+        if (arr[i] == target)
+        {
+            ifFound = true;
             break;
         }
-        cout << endl;
+        // cout << endl;
+    }
+    if (ifFound)
+    {
+        cout << "Element found at index: " << i << endl;
+    }
+    else
+    {
+        cout << "Element not found " << endl;
     }
 
     return 0;
