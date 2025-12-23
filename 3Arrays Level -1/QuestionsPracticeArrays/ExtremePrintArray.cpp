952874451a8ea98e41771fdf2c4297f742358dd1
rohitjha424 <rohitjha424@gmail.com>
2025-12-24 00:25:48 +0530

    Code optimized

--- 3Arrays Level -1/QuestionsPracticeArrays/ExtremePrintArray.cpp
+++ 3Arrays Level -1/QuestionsPracticeArrays/ExtremePrintArray.cpp
@@ -3,34 +3,31 @@
 #include <iostream>
 using namespace std;
 
-
 void extremePrint(int arr[], int size)
 {
     int left = 0;
     int right = size - 1;
     cout << "Extreme Print of given Array elements is: " << endl;
-    while (left < right)
+    while (left <= right)
     {
-        cout<<arr[left]<<" "<<arr[right]<<" ";
+        if (left == right)
+        {
+            cout << arr[left];
+        }
+        else
+        {
+            cout << arr[left] << " " << arr[right] << " ";
+        }
         left++;
         right--;
     }
-    if(left==right){
-        cout<<arr[left];
-    }
-    // Printing Swapped Array
-    // for (int i = 0; i < size; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
 }
-
 
 int main()
 {
 
-    int arr[] = {10, 20, 30, 40, 50, 60};
-    int size = 6;
+    int arr[] = {10, 20, 30, 40, 50, 60,70};
+    int size = 7;
 
     extremePrint(arr, size);
 
