b7bf44761cb22132f35ed571109c4046ed67fa34
rohitjha424 <rohitjha424@gmail.com>
2025-12-24 00:18:59 +0530

    ExtremeElementPrint Done

--- 3Arrays Level -1/QuestionsPracticeArrays/ExtremePrintArray.cpp
+++ 3Arrays Level -1/QuestionsPracticeArrays/ExtremePrintArray.cpp
@@ -1,7 +1,6 @@
 // Print Extreme Element of an Array
 
 #include <iostream>
-#include <limits.h>
 using namespace std;
 
 
@@ -10,11 +9,14 @@ void extremePrint(int arr[], int size)
     int left = 0;
     int right = size - 1;
     cout << "Extreme Print of given Array elements is: " << endl;
-    while (left <= right)
+    while (left < right)
     {
         cout<<arr[left]<<" "<<arr[right]<<" ";
         left++;
         right--;
+    }
+    if(left==right){
+        cout<<arr[left];
     }
     // Printing Swapped Array
     // for (int i = 0; i < size; i++)
@@ -28,7 +30,7 @@ int main()
 {
 
     int arr[] = {10, 20, 30, 40, 50, 60};
-    int size = 7;
+    int size = 6;
 
     extremePrint(arr, size);
 
