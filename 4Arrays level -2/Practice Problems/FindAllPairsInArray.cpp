546a22c89c0c62b99c13e139009b0ae1144eebb0
rohitjha424 <rohitjha424@gmail.com>
2025-12-26 01:46:04 +0530

    logic corrected

--- 4Arrays level -2/Practice Problems/FindAllPairsInArray.cpp
+++ 4Arrays level -2/Practice Problems/FindAllPairsInArray.cpp
@@ -1,4 +1,8 @@
 // Find pairs in an Array
+// int arr[] = {10,20,30};
+// [10,10] [10,20] [10,30] 
+// [20,10] [20,20] [20,30]
+// [30,10] [30,20] [30,30]
 
 #include <iostream>
 using namespace std;
@@ -8,23 +12,20 @@ void findAllPairs(int arr[], int size)
     int j = 0;
     for (int i = 0; i < size; i++)
     {
-      cout<<"["<<arr[i]<<","<<arr[j]<<"] ";
-      if((i==size-1)&&(j<size)){
-        j++;
-        i=0;
-      }
+        for (int j = 0; j < size; j++)
+        {
+            cout << "[" << arr[i] << "," << arr[j] << "] ";
+        }
+        cout << endl;
     }
-
 }
 
 int main()
 {
-
-    int arr[] = {10,20,30};
+    int arr[] = {10, 20, 30};
     int size = 3;
 
     findAllPairs(arr, size);
- 
 
     return 0;
 }
