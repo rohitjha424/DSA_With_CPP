7058a6074ff2dbead61e168d4892b8b63427b715
rohitjha424 <rohitjha424@gmail.com>
2025-12-25 23:10:41 +0530

    Attemting Find pairs Question

---
+++ 4Arrays level -2/Practice Problems/FindAllPairsInArray.cpp
@@ -0,0 +1,30 @@
+// Find pairs in an Array
+
+#include <iostream>
+using namespace std;
+
+void findAllPairs(int arr[], int size)
+{
+    int j = 0;
+    for (int i = 0; i < size; i++)
+    {
+      cout<<arr[i]<<","<<arr[j]<<endl;
+      if(i==size-1){
+        j++;
+        i=0;
+      }
+    }
+
+}
+
+int main()
+{
+
+    int arr[] = {10,20,30};
+    int size = 3;
+
+    findAllPairs(arr, size);
+ 
+
+    return 0;
+}
