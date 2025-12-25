4cb3e644000349ddbb0a920790e3164995bf981c
rohitjha424 <rohitjha424@gmail.com>
2025-12-25 23:02:39 +0530

    Formatting

--- 4Arrays level -2/Practice Problems/FindUniqueElement.cpp
+++ 4Arrays level -2/Practice Problems/FindUniqueElement.cpp
@@ -1,14 +1,14 @@
-//Find Unique elecment in an Array
+// Find Unique elecment in an Array
 
 #include <iostream>
 using namespace std;
 
 int findUnique(int arr[], int size)
 {
-   
     int ans = 0;
-    for(int i=0; i<size; i++){
-        ans = ans^arr[i];
+    for (int i = 0; i < size; i++)
+    {
+        ans = ans ^ arr[i];
     }
     return ans;
 }
@@ -16,11 +16,11 @@ int findUnique(int arr[], int size)
 int main()
 {
 
-    int arr[] = {2,10,11,13,10,2,15,13,15};
+    int arr[] = {2, 10, 11, 13, 10, 2, 15, 13, 15};
     int size = 9;
 
-    int finalAns= findUnique(arr, size);
+    int finalAns = findUnique(arr, size);
+    cout << "Unique Element in an given Array is: " << finalAns << endl;
 
-    cout << "Unique Element in an given Array is: " <<finalAns<< endl;
     return 0;
 }
