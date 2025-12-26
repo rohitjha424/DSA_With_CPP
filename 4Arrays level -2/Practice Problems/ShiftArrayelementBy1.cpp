3bf52d1668d9caab14af36b923adc82b27345584
rohitjha424 <rohitjha424@gmail.com>
2025-12-26 22:48:36 +0530

    Left Shift Logic Added

--- 4Arrays level -2/Practice Problems/ShiftArrayelementBy1.cpp
+++ 4Arrays level -2/Practice Problems/ShiftArrayelementBy1.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std;
 
-void shiftByOne(int arr[], int size)
+void rightShiftByOne(int arr[], int size)
 {
     int temp = arr[size - 1];
 
@@ -15,18 +15,46 @@ void shiftByOne(int arr[], int size)
     }
     arr[0] = temp;
 }
+void leftShiftByOne(int arr1[], int size)
+{
+    int temp = arr1[0];
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        arr1[i] = arr1[i + 1];
+    }
+    arr1[size - 1] = temp;
+}
 
 int main()
 {
     int arr[] = {10, 20, 30, 40, 50, 60};
+    int arr1[] = {10, 20, 30, 40, 50, 60};
     int size = 6;
 
-    shiftByOne(arr, size);
-
-    cout << "Array After Shifting Array Element by 1:" << endl;
+    cout << "Original Array:" << endl;
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // right shift result
+    rightShiftByOne(arr, size);
+    cout << "Array After Right Shifting Array Element by 1:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    // left shift result
+    leftShiftByOne(arr1, size);
+    cout << "Array After Left Shifting Array Element by 1:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr1[i] << " ";
     }
 
     return 0;
