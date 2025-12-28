33d43da685168f47be1b75738631b89637254f78
rohitjha424 <rohitjha424@gmail.com>
2025-12-28 15:12:41 +0530

    vector implimented

--- 5STL/STL_Intro.cpp
+++ 5STL/STL_Intro.cpp
@@ -1,39 +1,72 @@
 // STL Intro
 
 #include <iostream>
+#include<vector>
 using namespace std;
 
-void fun(int a[], int size)
+// void fun(int a[], int size)
+// {
+//     cout<<"printing Array element:"<<endl;
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << a[i] << endl;
+//     }
+// }
+
+void printVector(vector<int>v)
 {
-    cout<<"printing Array element:"<<endl;
+    int size = v.size(); // in-built function to tell us the size in vector.
+    cout<<"printing vector elements:"<<endl;
     for (int i = 0; i < size; i++)
     {
-        cout << a[i] << endl;
+        cout << v[i] << endl;
     }
 }
+
+
 
 int main()
 {
 
     // Static memmory Alllocation:
-    int arr[5] = {1, 2, 3, 4, 5};
-    fun(arr, 5);
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // fun(arr, 5);
 
-    // dynamic
-    int n;
-    cout << "Enter size of the dynamic Array:" << endl;
+    // // dynamic
+    // int n;
+    // cout << "Enter size of the dynamic Array:" << endl;
 
-    cin >> n;
-    int *arrary = new int[n]; // here by default , each element woild be zero or garbage.
+    // cin >> n;
+    // int *arrary = new int[n]; // here by default , each element woild be zero or garbage.
 
-    cout << "Enter elements of the dynamic Array:" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        int data;
-        cin >> data;
-        arrary[i] = data;
-    }
-    fun(arrary, n);
+    // cout << "Enter elements of the dynamic Array:" << endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int data;
+    //     cin >> data;
+    //     arrary[i] = data;
+    // }
+    //here if we try to insert more values, it will crash in error as the size here also defined.
+    // fun(arrary, n);
+
+
+    //vector
+    //in vector, dont tell the size of the vector
+    //just keep inserting, I will manage the allocations.
+
+    vector<int>v; // like an array >>size not decalred
+    
+    //insert in vector as mmuch as required. it will manage
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+
+    printVector(v);
+
 
     return 0;
 }
