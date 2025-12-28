fd27ec2b7c97afe59598222c44bdc0fad1aef5c5
rohitjha424 <rohitjha424@gmail.com>
2025-12-28 14:42:19 +0530

    dynamic reallocation example

--- 5STL/STL_Intro.cpp
+++ 5STL/STL_Intro.cpp
@@ -3,20 +3,36 @@
 #include <iostream>
 using namespace std;
 
-void fun(int a[], int size){
-    for(int i=0; i<size; i++){
-        cout<<a[i]<<endl;
+void fun(int a[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << a[i] << endl;
     }
-
 }
 
-int main(){
+int main()
+{
 
+    // Static memmory Alllocation:
+    int arr[5] = {1, 2, 3, 4, 5};
+    fun(arr, 5);
 
-    //Static memmory Alllocation:
-    int arr[5] ={1,2,3,4,5};
-    fun(arr,5);
+    // dynamic
+    int n;
+    cout << "Enter size of the dynamic Array:" << endl;
 
+    cin >> n;
+    int *arrary = new int[n]; // here by default , each element woild be zero or garbage.
 
+    cout << "Enter elements of the dynamic Array:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int data;
+        cin >> data;
+        arrary[i] = data;
+    }
+    fun(arrary, n);
+    
     return 0;
 }
