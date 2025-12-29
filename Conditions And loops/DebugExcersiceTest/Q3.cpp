57571232984464507b946ed148d12e2cc415f310
rohitjha424 <rohitjha424@gmail.com>
2025-12-29 23:52:22 +0530

    formatting

--- Conditions And loops/DebugExcersiceTest/Q3.cpp
+++ Conditions And loops/DebugExcersiceTest/Q3.cpp
@@ -1,4 +1,4 @@
-//3. Debug the code. It is trying to print the given pattern.
+// 3. Debug the code. It is trying to print the given pattern.
 
 /*
 Pattern
@@ -8,29 +8,34 @@ N = 4
  *****
 *******
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int i=1;
-    while(i<=n){
-        int j = i,count = 2*i-1,gaps=(n-i),k=1;
-        while(k<=gaps){
-            cout<<" ";
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = i, count = 2 * i - 1, gaps = (n - i), k = 1;
+        while (k <= gaps)
+        {
+            cout << " ";
             k = k + 1;
         }
-        int m = 1,ch = count;
-        while(m<=ch){
-            cout<<"*";
+        int m = 1, ch = count;
+        while (m <= ch)
+        {
+            cout << "*";
             m = m + 1;
         }
         k = 1;
-        while(k<=gaps){
-            cout<<" ";
+        while (k <= gaps)
+        {
+            cout << " ";
             k = k + 1;
         }
-        cout<<"\n";
+        cout << "\n";
         i = i + 1;
     }
 }
