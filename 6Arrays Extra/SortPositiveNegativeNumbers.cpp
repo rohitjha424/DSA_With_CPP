30a4f5f7592ad6ec34ba0cfa540b298ae9a70ce0
rohitjha424 <rohitjha424@gmail.com>
2025-12-30 22:23:39 +0530

    Attempting with two pointer Approch

---
+++ 6Arrays Extra/SortPositiveNegativeNumbers.cpp
@@ -0,0 +1,47 @@
+// Sort Zeroes and Ones in an Array
+
+#include <iostream>
+using namespace std;
+
+void sortNegativeAndPositive(int arr[], int size)
+{
+    int negativeCount = 0;
+    int positiveCount = 0;
+
+   int j;
+    for (j = 0; j < size; i++){
+            if(arr[j]>0)
+            break;
+        }
+
+    for (int i = 0; i < size; i++)
+    {
+        
+    }
+
+    int i;
+    for (i = 0; i < negativeCount; i++)
+    {
+        arr[i] = 0;
+    }
+    for (int j = i; j < size; j++)
+    {
+        arr[j] = 1;
+    }
+}
+
+int main()
+{
+    int arr[] = {23,-7,12,-10,-1,40,60};
+    int size = 7;
+
+    sortNegativeAndPositive(arr, size);
+
+    cout << "Array After Sorting:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
