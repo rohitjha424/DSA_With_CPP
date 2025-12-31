1b964298699a2d0b7fd7b1ec2c8b9d2e5efd98b8
rohitjha424 <rohitjha424@gmail.com>
2025-12-31 23:25:00 +0530

    formatting

--- 6Arrays Extra/SortPositiveNegativeNumbers.cpp
+++ 6Arrays Extra/SortPositiveNegativeNumbers.cpp
@@ -5,22 +5,20 @@ using namespace std;
 
 void sortNegativeAndPositive(int arr[], int size)
 {
-
-   int j=0;
+    int j = 0;
     for (int i = 0; i < size; i++)
     {
-        if(arr[i]<0){
-            swap(arr[i],arr[j]);
+        if (arr[i] < 0)
+        {
+            swap(arr[i], arr[j]);
             j++;
         }
-        
     }
-
 }
 
 int main()
 {
-    int arr[] = {23,-7,12,-10,-1,40,60};
+    int arr[] = {23, -7, 12, -10, -1, 40, 60};
     int size = 7;
 
     sortNegativeAndPositive(arr, size);
