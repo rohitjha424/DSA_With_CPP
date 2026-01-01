e8435970039a5669f67286bea5614e86fe1ea534
rohitjha424 <rohitjha424@gmail.com>
2026-01-02 01:51:55 +0530

    Best Solution kept

--- 3Arrays Level -1/QuestionsPracticeArrays/FindMaxNumInArray.cpp
+++ 3Arrays Level -1/QuestionsPracticeArrays/FindMaxNumInArray.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 // Mthod 1 : Best Practice to use INT_MAX
-// void findMax(int arr[], int size)
-// {
-//     int maxNum = INT_MIN;
-//     for (int i = 0; i < size; i++)
-//     {
-//         if (arr[i] > maxNum)
-//         {
-//             maxNum = arr[i];
-//         }
-//     }
-//     cout << "Maximum Number in the given Array is: " << maxNum << endl;
-// }
+void findMax(int arr[], int size)
+{
+    int maxNum = INT_MIN;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > maxNum)
+        {
+            maxNum = arr[i];
+        }
+    }
+    cout << "Maximum Number in the given Array is: " << maxNum << endl;
+}
 
 // Method 2: Initialize minMun with first Array element and then compare further
 // void findMax(int arr[], int size)
@@ -33,15 +33,15 @@ using namespace std;
 // }
 
 // Mthod 3 : using inbuilt Function min()
-void findMax(int arr[], int size)
-{
-    int maxNum =0;
-    for (int i = 0; i < size; i++)
-    {
-        maxNum = max(arr[i], maxNum);
-    }
-    cout << "Maximum Number in the given Array is: " << maxNum << endl;
-}
+// void findMax(int arr[], int size)
+// {
+//     int maxNum =0;
+//     for (int i = 0; i < size; i++)
+//     {
+//         maxNum = max(arr[i], maxNum);
+//     }
+//     cout << "Maximum Number in the given Array is: " << maxNum << endl;
+// }
 
 int main()
 {
