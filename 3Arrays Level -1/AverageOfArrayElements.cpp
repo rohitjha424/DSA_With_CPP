215ac08a58ec73416f6f9e550e0c32e7c1b0a1c0
rohitjha424 <rohitjha424@gmail.com>
2026-01-01 23:27:35 +0530

    AvgElement solved

---
+++ 3Arrays Level -1/AverageOfArrayElements.cpp
@@ -0,0 +1,24 @@
+// Average of All elements in an Array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[] = {1,2,3,4,5};
+    double n = 5;
+    double totalSum=0;
+    double avg = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+       totalSum = totalSum +arr[i];
+        // cout << endl;
+    }
+    avg = totalSum/n;
+
+        cout <<  "Average of All elements in an Array is: " << avg << endl;
+    
+    return 0;
+}
