6ca7ebfc88ec2445f9202205af850ae5b0081009
rohitjha424 <rohitjha424@gmail.com>
2026-01-01 23:28:02 +0530

    formatting

--- 3Arrays Level -1/AverageOfArrayElements.cpp
+++ 3Arrays Level -1/AverageOfArrayElements.cpp
@@ -5,20 +5,18 @@ using namespace std;
 
 int main()
 {
-
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {1, 2, 3, 4, 5};
     double n = 5;
-    double totalSum=0;
+    double totalSum = 0;
     double avg = 0;
 
     for (int i = 0; i < n; i++)
     {
-       totalSum = totalSum +arr[i];
-        // cout << endl;
+        totalSum = totalSum + arr[i];
     }
-    avg = totalSum/n;
+    avg = totalSum / n;
 
-        cout <<  "Average of All elements in an Array is: " << avg << endl;
-    
+    cout << "Average of All elements in an Array is: " << avg << endl;
+
     return 0;
 }
