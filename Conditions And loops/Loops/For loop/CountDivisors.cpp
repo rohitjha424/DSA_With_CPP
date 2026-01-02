ad26ec6b20804900a6ea55575aacc993162d1bc5
rohitjha424 <rohitjha424@gmail.com>
2026-01-02 16:51:29 +0530

    count divisors

---
+++ Conditions And loops/Loops/For loop/CountDivisors.cpp
@@ -0,0 +1,21 @@
+// Count all divisors of a given Number.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    int count =0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            count++;
+        }
+    }
+    cout<<"Total Number of divisors: "<<count<<endl;
+    return 0;
+}
