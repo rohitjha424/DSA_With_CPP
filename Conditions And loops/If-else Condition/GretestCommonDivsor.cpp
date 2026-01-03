0b289584467c6f475bbe0d8268d7e476008da624
rohitjha424 <rohitjha424@gmail.com>
2026-01-03 22:48:11 +0530

    Solved using Equilids Method

--- Conditions And loops/If-else Condition/GretestCommonDivsor.cpp
+++ Conditions And loops/If-else Condition/GretestCommonDivsor.cpp
@@ -6,27 +6,44 @@ using namespace std;
 int main()
 {
     int num1, num2;
-    cout<<"Enter two Numbers"<<endl;
-    cin >> num1>>num2;
+    cout << "Enter two Numbers" << endl;
+    cin >> num1 >> num2;
 
-    int checkBig;
-    if(num1>=num2){
-        checkBig = num1;
-    }
-    else {
-        checkBig =num2;
-    }
-    int gcd=0;
+    // int checkBig;
+    // if(num1>=num2){
+    //     checkBig = num1;
+    // }
+    // else {
+    //     checkBig =num2;
+    // }
+    // int gcd=0;
 
-    for(int i=2 ; i<checkBig; i++){
-        if((num1%i==0) && (num2%i==0)){
-            if(i>gcd){
-                gcd =i;
-            }
+    // for(int i=2 ; i<checkBig; i++){
+    //     if((num1%i==0) && (num2%i==0)){
+    //         if(i>gcd){
+    //             gcd =i;
+    //         }
+    //     }
+    // }
+    // cout<<"GCD of given numbers is: "<<gcd;
+
+    // Using Equilids Method:
+    num1 = abs(num1);
+    num2 = abs(num2);
+
+    // Using Euclidean Method (Subtraction)
+    while (num1 != num2)
+    {
+        if (num1 > num2)
+        {
+            num1 = num1 - num2;
+        }
+        else
+        {
+            num2 = num2 - num1;
         }
     }
-    cout<<"GCD of given numbers is: "<<gcd;
 
-   
+    cout << "GCD of given numbers is: " << num1;
     return 0;
 }
