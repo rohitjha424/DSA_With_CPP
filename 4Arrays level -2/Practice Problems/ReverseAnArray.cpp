a5664192c0d5ca3528bf9bca84ec68c604311c03
rohitjha424 <rohitjha424@gmail.com>
2026-01-07 22:58:13 +0530

    reverse of array using swap method

---
+++ 4Arrays level -2/Practice Problems/ReverseAnArray.cpp
@@ -0,0 +1,33 @@
+// Reverse an Array
+
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size)
+{
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right)
+    {
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
+    int size = 8;
+
+    reverseArray(arr, size);
+
+    cout << "Array After reversing:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
