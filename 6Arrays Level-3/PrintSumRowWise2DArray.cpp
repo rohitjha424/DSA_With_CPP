7faa2d5522a21541b7d15d37aa4bdec9059cd3b6
rohitjha424 <rohitjha424@gmail.com>
2026-01-11 02:33:08 +0530

    row wise sum for 2D Array Solved

---
+++ 6Arrays Level-3/PrintSumRowWise2DArray.cpp
@@ -0,0 +1,46 @@
+// 2 D array Row Wise Sum
+#include <iostream>
+using namespace std;
+
+void rowWiseSum2DArray(int arr[3][3])
+{
+
+    // row wise sum:
+    cout << "Row Wise traversal of given 2D Array:" << endl;
+    for (int row = 0; row < 3; row++)
+    {
+        int sum = 0;
+        for (int col = 0; col < 3; col++)
+        {
+            sum = sum + arr[row][col];
+        }
+        cout << "Sum for row " << row << " is: " << sum << endl;
+    }
+}
+
+int main()
+{
+
+    // create a 2D Array:
+
+    int arr[3][3] =
+        {10, 20, 30,
+         40, 50, 60,
+         70, 80, 90};
+
+    // Printing 2D Array elements
+    cout << endl
+         << "2D Array:" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    rowWiseSum2DArray(arr);
+
+    return 0;
+}
