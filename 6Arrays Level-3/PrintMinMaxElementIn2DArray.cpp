fce7eb599420c080808b61be371dd59a7cabfe86
rohitjha424 <rohitjha424@gmail.com>
2026-01-12 23:52:19 +0530

    Max Element in 2D Array Done

--- 6Arrays Level-3/PrintMinMaxElementIn2DArray.cpp
+++ 6Arrays Level-3/PrintMinMaxElementIn2DArray.cpp
@@ -1,8 +1,10 @@
 // Print Min And Max element in an 2D Array
+
 #include <iostream>
 #include <limits.h>
 using namespace std;
 
+// Min Element
 int minElementIn2DArray(int arr[][3], int m, int n)
 {
 
@@ -18,11 +20,23 @@ int minElementIn2DArray(int arr[][3], int m, int n)
     return minElement;
 }
 
+// Max Element
+int maxElementIn2DArray(int arr[][3], int m, int n)
+{
+    int maxElement = INT_MIN;
+    for (int row = 0; row < m; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            if (arr[row][col] > maxElement)
+                maxElement = arr[row][col];
+        }
+    }
+    return maxElement;
+}
 int main()
 {
-
     // create a 2D Array:
-
     int arr[3][3] =
         {{10, 20, 30},
          {40, 50, 60},
@@ -42,6 +56,9 @@ int main()
 
     int minElement = minElementIn2DArray(arr, 3, 3);
     cout << "The minimum element in the 2D Array is: " << minElement << endl;
+
+    int maxElement = maxElementIn2DArray(arr, 3, 3);
+    cout << "The maximum element in the 2D Array is: " << maxElement << endl;
 
     return 0;
 }
