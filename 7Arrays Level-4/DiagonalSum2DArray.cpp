a515fee4f01bb8c829f185fc24e7d5e6c8352369
rohitjha424 <rohitjha424@gmail.com>
2026-01-13 23:13:08 +0530

    Formatting

--- 7Arrays Level-4/DiagonalSum2DArray.cpp
+++ 7Arrays Level-4/DiagonalSum2DArray.cpp
@@ -1,19 +1,18 @@
-//Diagonal Sum in 2D Array
+// Diagonal Sum in 2D Array
 #include <iostream>
 #include <limits.h>
 using namespace std;
 
-
 void diagonalSum2DArray(int arr[][3], int size)
 {
-
     int diagonalSum1 = 0;
     for (int row = 0; row < size; row++)
     {
         for (int col = 0; col < size; col++)
         {
-            if (row == col){
-                diagonalSum1 = diagonalSum1+arr[row][col];
+            if (row == col)
+            {
+                diagonalSum1 = diagonalSum1 + arr[row][col];
             }
         }
     }
@@ -23,20 +22,22 @@ void diagonalSum2DArray(int arr[][3], int size)
     {
         for (int col = 0; col < size; col++)
         {
-            if (row + col == size-1 ){
-                if(size%2!=0 && row==col){
+            if (row + col == size - 1)
+            {
+                if (size % 2 != 0 && row == col)
+                {
                     continue;
                 }
-                else{
+                else
+                {
                     diagonalSum2 += arr[row][col];
                 }
             }
         }
     }
 
-    cout<<diagonalSum1+ diagonalSum2;
+    cout << diagonalSum1 + diagonalSum2;
 }
-
 
 int main()
 {
@@ -57,11 +58,6 @@ int main()
         }
         cout << endl;
     }
-
-     diagonalSum2DArray(arr, 3);
-    // int diagonalSum = diagonalSum2DArray(arr, 3);
-    // cout << "Diagona Sum: " << diagonalSum << endl;
-
-  
+    diagonalSum2DArray(arr, 3);
     return 0;
 }
