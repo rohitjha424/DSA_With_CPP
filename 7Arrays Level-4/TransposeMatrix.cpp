5bea6730f6e8a94bae0192e71f2a186b135c2e94
rohitjha424 <rohitjha424@gmail.com>
2026-01-14 02:23:45 +0530

    Formatting and removed unnessary code

--- 7Arrays Level-4/TransposeMatrix.cpp
+++ 7Arrays Level-4/TransposeMatrix.cpp
@@ -1,22 +1,32 @@
 // Transpose Matrix
+// 2D Original Array:
+// 10 20 30
+// 40 50 60
+// 70 80 90 
+// 2D Array after doing Transpose of Matrix:
+// 10 40 70
+// 20 50 80
+// 30 60 90
+
 #include <iostream>
-#include <limits.h>
 using namespace std;
 
 void transposeMatrix(int arr[][3], int size)
 {
     // int sum =0;
-    for(int i=0; i<size; i++){
-        for(int j=i+1; j<size; j++){
-
-            if(i != j){
-                cout<<i<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (i != j)
+            {
                 swap(arr[i][j], arr[j][i]);
             }
         }
     }
-    cout << endl
-         << "2D Array:" << endl;
+
+    cout << endl<< "2D Array after doing Transpose of Matrix:" << endl;
+
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -37,7 +47,7 @@ int main()
 
     // Printing 2D Array elements
     cout << endl
-         << "2D Array:" << endl;
+         << "2D Original Array:" << endl;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -47,6 +57,6 @@ int main()
         cout << endl;
     }
     transposeMatrix(arr, 3);
-    
+
     return 0;
 }
