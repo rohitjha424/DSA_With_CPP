acafdae59ec0adc62dcf38782523c1c95c056e05
rohitjha424 <rohitjha424@gmail.com>
2026-01-18 23:36:19 +0530

    Q1 Done

---
+++ 7Arrays Level-4/DebugExcercise/ArraySum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, sum=0;
+    cin >> n;
+    int input[n];
+    for(int i=0;i<n;i++){
+        cin >> input[i];
+    }
+     for(int i=0; i<n; i++){
+         sum = sum + input[i];
+     }
+    cout << sum << endl;
+    return 0;
+}
