ff6360ae5c7f4fa26d0a81866c14ec62a5ef0f2e
rohitjha424 <rohitjha424@gmail.com>
2026-01-19 23:21:22 +0530

    Proper Logic Added

--- 7Arrays Level-4/DebugExcercise/Q2.cpp
+++ 7Arrays Level-4/DebugExcercise/Q2.cpp
@@ -1,24 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int linearSearch(int arr[], int n ,int val){
-    for(int i=0;i<n;i++){
-        if(arr[i]==val){
+int linearSearch(int arr[], int n, int val)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == val)
+        {
             return i;
         }
     }
     return -1;
 }
-int main(){
-    int n, sum=0;
+int main()
+{
+    int n, sum = 0;
+    cout << "Enter the length of array" << endl;
     cin >> n;
     int input[n];
-    for(int i=0;i<n;i++){
+    cout << "Enter Elements" << endl;
+    for (int i = 0; i < n; i++)
+    {
         cin >> input[i];
     }
     int val;
-    cin>>val;
-    linearSearch(input, n, val);
- 
+    cout << "Enter value to search" << endl;
+    cin >> val;
+    int found = linearSearch(input, n, val);
+    if (found < 0)
+    {
+        cout << "Not Found";
+        return 0;
+    }
+    cout << "Found at: " << found + 1 << " position";
     return 0;
 }
