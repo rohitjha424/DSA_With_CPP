dc66a8ea7c4eb15c7f3bc5006f72bc830d22f23d
rohitjha424 <rohitjha424@gmail.com>
2026-01-20 23:53:16 +0530

    formatting

--- 7Arrays Level-4/DebugExcercise/Q1.cpp
+++ 7Arrays Level-4/DebugExcercise/Q1.cpp
@@ -1,17 +1,20 @@
-//Sum of array.
-#include<iostream>
+// Sum of array.
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n, sum=0;
+int main()
+{
+    int n, sum = 0;
     cin >> n;
     int input[n];
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> input[i];
     }
-     for(int i=0; i<n; i++){
-         sum = sum + input[i];
-     }
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + input[i];
+    }
     cout << sum << endl;
     return 0;
 }
