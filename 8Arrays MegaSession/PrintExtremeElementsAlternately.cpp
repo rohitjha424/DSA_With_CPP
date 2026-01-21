ac664dfdb8c82cc91c8a3f1d8f77eceb7b7c00f1
rohitjha424 <rohitjha424@gmail.com>
2026-01-21 23:59:17 +0530

    formatting

--- 8Arrays MegaSession/PrintExtremeElementsAlternately.cpp
+++ 8Arrays MegaSession/PrintExtremeElementsAlternately.cpp
@@ -1,32 +1,36 @@
-//Print Extreme Elements Alternately
-// Original Array:
-// 1 3 2 4 5
-// Sorted Array:
-// 1 2 3 4 5
-// Printing the Extreme Elements Alternately
-// 1 5 2 4 3
+// Print Extreme Elements Alternately
+//  Original Array:
+//  1 3 2 4 5
+//  Sorted Array:
+//  1 2 3 4 5
+//  Printing the Extreme Elements Alternately
+//  1 5 2 4 3
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-void PrintExtremeElementsAlternately(vector<int>& arr, int size){
+void PrintExtremeElementsAlternately(vector<int> &arr, int size)
+{
     sort(arr.begin(), arr.end());
-     cout <<"Sorted Array:"<<endl;
+    cout << "Sorted Array:" << endl;
     for (int i = 0; i < size; i++)
     {
-        cout << arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 
-    int Max_element = size-1;
+    int Max_element = size - 1;
     int Min_element = 0;
-    cout<<"Printing the Extreme Elements Alternately"<<endl;
-    while(Min_element<=Max_element){
-        cout<<arr[Min_element]<<" ";
-        if(Min_element !=Max_element){
-            cout<<arr[Max_element]<<" ";
+
+    cout << "Printing the Extreme Elements Alternately" << endl;
+    while (Min_element <= Max_element)
+    {
+        cout << arr[Min_element] << " ";
+        if (Min_element != Max_element)
+        {
+            cout << arr[Max_element] << " ";
         }
         Min_element++;
         Max_element--;
@@ -34,15 +38,17 @@ void PrintExtremeElementsAlternately(vector<int>& arr, int size){
 }
 int main()
 {
-    vector<int> arr = {1,3,2,4,5};
+    vector<int> arr = {1, 3, 2, 4, 5};
     int size = arr.size();
-    cout <<"Original Array:"<<endl;
+    
+    cout << "Original Array:" << endl;
     for (int i = 0; i < size; i++)
     {
-        cout << arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 
-   PrintExtremeElementsAlternately(arr, size);
+    PrintExtremeElementsAlternately(arr, size);
+
     return 0;
 }
