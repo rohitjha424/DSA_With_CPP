320919008026fa35cfff416deb05c1c0c07d0292
rohitjha424 <rohitjha424@gmail.com>
2026-01-24 23:28:23 +0530

    Logic Corrected Little bit

--- 8Arrays MegaSession/FindModeOfArray.cpp
+++ 8Arrays MegaSession/FindModeOfArray.cpp
@@ -8,9 +8,9 @@ using namespace std;
 void findMode(vector<int> &arr, int size)
 {
 
-   sort(arr.begin() , arr.end());
-   cout<<"Sorted Array is :"<<endl;
-   for (int i = 0; i < size; i++)
+    sort(arr.begin(), arr.end());
+    cout << "Sorted Array is :" << endl;
+    for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
@@ -19,25 +19,29 @@ void findMode(vector<int> &arr, int size)
     int mode = arr[0];
     int currentFreq = 1;
 
-    for(int i=1; i<size; i++){
-        if(arr[i] ==arr[i-1]){
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] == arr[i - 1])
+        {
             currentFreq++;
         }
-        else{
-            currentFreq =1;
+        else
+        {
+            currentFreq = 1;
         }
 
-        if(currentFreq>maxFreq){
+        if (currentFreq > maxFreq)
+        {
             maxFreq = currentFreq;
             mode = arr[i];
         }
     }
-    cout<<"Mode is:"<<mode<<endl;
-
+    cout << endl
+         << "Mode is:" << mode << endl;
 }
 int main()
 {
-    vector<int> arr = {1,2,3,4,3,5,3,6,3,7};
+    vector<int> arr = {1, 2, 3, 4, 3, 5, 3, 6, 3, 7};
     int size = arr.size();
 
     cout << "Original Array:" << endl;
@@ -48,7 +52,6 @@ int main()
     cout << endl;
 
     findMode(arr, size);
-    // cout << "The  First Unsorted Element In Array is at: " << ans << "th index" << endl;
 
     return 0;
 }
