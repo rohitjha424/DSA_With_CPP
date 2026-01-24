c82a3c43329286d3bdeab2e07878bf2961b54168
rohitjha424 <rohitjha424@gmail.com>
2026-01-24 23:49:42 +0530

    HighestFrequesncy Part Done

--- 8Arrays MegaSession/FindHighestAndLowestFrequencyInArray.cpp
+++ 8Arrays MegaSession/FindHighestAndLowestFrequencyInArray.cpp
@@ -1,1 +1,56 @@
 //Find Highest And Lowest Frequency Seperately.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void findMode(vector<int> &arr, int size)
+{
+
+    sort(arr.begin(), arr.end());
+    cout << "Sorted Array is :" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    int maxFreq = 1;
+    int mode = arr[0];
+    int currentFreq = 1;
+
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] == arr[i - 1])
+        {
+            currentFreq++;
+        }
+        else
+        {
+            currentFreq = 1;
+        }
+
+        if (currentFreq > maxFreq)
+        {
+            maxFreq = currentFreq;
+            mode = arr[i];
+        }
+    }
+    cout << endl
+         << "Mode is:" << mode << endl;
+}
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 3, 5, 3, 6, 3, 7};
+    int size = arr.size();
+
+    cout << "Original Array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    findMode(arr, size);
+
+    return 0;
+}
