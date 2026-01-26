4408d0c136bfb7e8a56a4a8577f2f2ace5201199
rohitjha424 <rohitjha424@gmail.com>
2026-01-26 19:47:27 +0530

    .begin()

--- 9STL/1Containers/vector.cpp
+++ 9STL/1Containers/vector.cpp
@@ -7,11 +7,13 @@ using namespace std;
 int main()
 {
     // creation
-    vector<int> marks; // will create a vector with named marks
-
+    vector<int> marks(5, -1); 
     // creation with size
-    vector<int> miles[20]; // will create a vector names miles with size 20.
+    // vector<int> miles[20]; // will create a vector names miles with size 20.
 
-    // creating vector with the initialization
-    vector<int> distances(15, 0); // will create vector named distances with size 15 and 0 in all indexes.
+    // // creating vector with the initialization
+    // vector<int> distances(15, 0); // will create vector named distances with size 15 and 0 in all indexes.
+
+    cout<<*(marks.begin())<<endl; // returns first element of vector 
+
 }
