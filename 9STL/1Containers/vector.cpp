28abc40c2b0905881f2be2495372d31dbd29bb1a
rohitjha424 <rohitjha424@gmail.com>
2026-01-26 19:40:35 +0530

    Vector Creation

--- 9STL/1Containers/vector.cpp
+++ 9STL/1Containers/vector.cpp
@@ -1,18 +1,17 @@
 // Learning About Vectors
 
-#include<iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
-int main(){
-
-    //creation
-    vector<int> marks;  // will create a vector with named marks
+int main()
+{
+    // creation
+    vector<int> marks; // will create a vector with named marks
 
     // creation with size
-    vector<int> miles[20];  // will create a vector names miles with size 20.
+    vector<int> miles[20]; // will create a vector names miles with size 20.
 
-    //creating vector with the initialization
-
-    vector<int> distances(15,0); // will create vector named distances with size 15 and 0 in all indexes.
+    // creating vector with the initialization
+    vector<int> distances(15, 0); // will create vector named distances with size 15 and 0 in all indexes.
 }
