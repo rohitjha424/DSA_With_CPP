5fb0c92c320918236e56951d7de045fdab1da86b
rohitjha424 <rohitjha424@gmail.com>
2026-01-26 19:40:09 +0530

    Started with Vectors in STL.

---
+++ 9STL/1Containers/vector.cpp
@@ -0,0 +1,18 @@
+// Learning About Vectors
+
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    //creation
+    vector<int> marks;  // will create a vector with named marks
+
+    // creation with size
+    vector<int> miles[20];  // will create a vector names miles with size 20.
+
+    //creating vector with the initialization
+
+    vector<int> distances(15,0); // will create vector named distances with size 15 and 0 in all indexes.
+}
