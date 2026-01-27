41710a4b9dcd6a6db5486d43e172786000c61e5b
rohitjha424 <rohitjha424@gmail.com>
2026-01-27 23:47:23 +0530

    comments uptated

--- 9STL/1Containers/list.cpp
+++ 9STL/1Containers/list.cpp
@@ -11,22 +11,22 @@ int main()
 
     //insertion
     //insertion from back:
-    myList.push_back(10);
-    myList.push_back(20);
-    myList.push_back(30);
-    myList.push_back(40);
+    myList.push_back(10);   //10
+    myList.push_back(20);   //10->20
+    myList.push_back(30);   //10->20->30
+    myList.push_back(40);   //10->20->30->40
 
     //insertion fron front:
-    myList.push_front(4);
-    myList.push_front(3);
-    myList.push_front(2);
-    myList.push_front(1);
+    myList.push_front(4);   //4->10->20->30->40
+    myList.push_front(3);   //3->4->10->20->30->40
+    myList.push_front(2);   //2->3->4->10->20->30->40
+    myList.push_front(1);   //1->2->3->4->10->20->30->40
 
     // *** deletion **
     //deletion from back
-    myList.pop_back();
+    myList.pop_back();       //1->2->3->4->10->20->30
     //deletion from front
-    myList.pop_front();
+    myList.pop_front();      //2->3->4->10->20->30
 
 
 
