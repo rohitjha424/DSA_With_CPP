b982a9ef05bb81be99ba6fbf01abdb026ea39587
rohitjha424 <rohitjha424@gmail.com>
2026-01-27 23:18:33 +0530

    isertion from frront

--- 9STL/1Containers/list.cpp
+++ 9STL/1Containers/list.cpp
@@ -10,10 +10,21 @@ int main()
     list<int> myList;
 
     //insertion
+    //insertion from back:
     myList.push_back(10);
     myList.push_back(20);
     myList.push_back(30);
     myList.push_back(40);
+
+    //insertion fron front:
+    myList.push_front(4);
+    myList.push_front(3);
+    myList.push_front(2);
+    myList.push_front(1);
+
+    
+
+
 
 
 
