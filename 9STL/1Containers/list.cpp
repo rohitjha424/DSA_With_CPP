569c238ba97b28710c15bc10a7785fafc78bcda6
rohitjha424 <rohitjha424@gmail.com>
2026-01-27 23:16:34 +0530

    List Creation and Insertion

--- 9STL/1Containers/list.cpp
+++ 9STL/1Containers/list.cpp
@@ -1,2 +1,25 @@
-//Learning About Lists in CPP
+// Learning About Lists in CPP
 
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+    // Creation Of List
+    list<int> myList;
+
+    //insertion
+    myList.push_back(10);
+    myList.push_back(20);
+    myList.push_back(30);
+    myList.push_back(40);
+
+
+
+
+
+
+
+    return 0;
+}
