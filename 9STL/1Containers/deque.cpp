73cc180bdf9042214faefbb40cfe87a43ca515c0
rohitjha424 <rohitjha424@gmail.com>
2026-01-29 01:45:04 +0530

    deque Creation

---
+++ 9STL/1Containers/deque.cpp
@@ -0,0 +1,16 @@
+// Learning About deque in CPP
+// we can have insertion and deletions operations on both front and back.
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main()
+{
+
+    //creation
+    deque<int> dq;
+    
+
+    return 0;
+}
