1ab0521988b84e5f150c0df0ccaf876566801d0d
rohitjha424 <rohitjha424@gmail.com>
2026-01-28 23:57:47 +0530

    creation of stack

--- 9STL/1Containers/stack.cpp
+++ 9STL/1Containers/stack.cpp
@@ -2,11 +2,17 @@
 // It Follows Last In first Out(LIFO)
 
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 int main()
 {
+    //creation
+    stack<int> st;
+
+
+
 
     return 0;
 }
