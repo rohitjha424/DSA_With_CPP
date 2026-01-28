fbc2e063ffe77dd7532ab9585e0d8c0d626708b0
rohitjha424 <rohitjha424@gmail.com>
2026-01-29 02:37:59 +0530

    insertion on back and front

--- 9STL/1Containers/deque.cpp
+++ 9STL/1Containers/deque.cpp
@@ -10,7 +10,18 @@ int main()
 
     //creation
     deque<int> dq;
-    
+
+    //insertion on back
+    dq.push_back(10);   //10
+    dq.push_back(20);   //10,20
+    dq.push_back(30);   //10,20,30
+    dq.push_back(40);   //10,20,30,40
+
+    //insertion on front
+    dq.push_front(1);   //1,10,20,30,40
+    dq.push_front(2);   //2,1,10,20,30,40
+    dq.push_front(3);   //3,2,1,10,20,30,40
+    dq.push_front(4);   //4,3,2,1,10,20,30,40
 
     return 0;
 }
