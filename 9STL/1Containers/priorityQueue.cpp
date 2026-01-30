576b8fb17e9f0e719cfd71f0a77128be5b9d9f47
rohitjha424 <rohitjha424@gmail.com>
2026-01-30 22:49:30 +0530

    insersion and size() in pq

--- 9STL/1Containers/priorityQueue.cpp
+++ 9STL/1Containers/priorityQueue.cpp
@@ -10,6 +10,13 @@ int main()
     //creation
     priority_queue<int> pq;
 
+    //insertion of values 
+    pq.push(10);  //10
+    pq.push(20);  //10,20
+    pq.push(30);  //10,20,30
+    pq.push(40);  //10,20,30,40
+
+    cout<<"Priorty Queue Size is : "<<pq.size()<<endl;  // will print Priority Queue Size
 
 
 
