8983dee1550544923d6fb49c02cc635ad443955e
rohitjha424 <rohitjha424@gmail.com>
2026-01-30 22:55:59 +0530

    order Corrected

--- 9STL/1Containers/priorityQueue.cpp
+++ 9STL/1Containers/priorityQueue.cpp
@@ -9,14 +9,16 @@ int main()
 
     //creation
     priority_queue<int> pq;
+    //by default>> max-heap ->max value ->highest priority
 
     //insertion of values 
     pq.push(10);  //10
-    pq.push(20);  //10,20
-    pq.push(30);  //10,20,30
-    pq.push(40);  //10,20,30,40
+    pq.push(20);  //20,10
+    pq.push(40);  //40,10,20
+    pq.push(30);  //40,30,20,10
 
     cout<<"Priorty Queue Size is : "<<pq.size()<<endl;  // will print Priority Queue Size
+
 
 
 
