b3634b425d4dab417d43d41e71e50576ca27e434
rohitjha424 <rohitjha424@gmail.com>
2026-01-30 23:50:14 +0530

    creation of unordered map

--- 9STL/1Containers/map.cpp
+++ 9STL/1Containers/map.cpp
@@ -2,10 +2,15 @@
 
 #include <iostream>
 #include<map>
+#include<unordered_map>
 using namespace std;
 
 int main()
 {
+
+    //creation of unordered map
+    unordered_map<string,string> table;
+
 
     return 0;
 }
