64d4206ec1522c301d9e238d7075805fd98d4ba5
rohitjha424 <rohitjha424@gmail.com>
2026-02-01 22:54:21 +0530

    Solved

--- 7Arrays Level-4/DebugExcercise/Q4.cpp
+++ 7Arrays Level-4/DebugExcercise/Q4.cpp
@@ -6,24 +6,24 @@ using namespace std;
 void swapAlternate(int arr[],int size){
     for(int i=0;i<size-1;i=i+2){
         int temp = arr[i];
-        arr[i+1] = arr[i];
-        arr[i] = temp;
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
     }
 }
 
 int main()
 {
-    int arr[5] = {1,2,3,4,5};
+    int arr[6] = {3, 9, 12, 6, 32, 4};
 
     cout<<"Original Array:"<<endl;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 6; i++)
     {
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-    swapAlternate(arr, 5);
+    swapAlternate(arr, 6);
     cout<<"the Array after swapping is:"<<endl;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 6; i++)
     {
         cout<<arr[i]<<" ";
     }
