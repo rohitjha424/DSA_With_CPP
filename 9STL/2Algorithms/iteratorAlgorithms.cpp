4051eab5037e8716d0aa9224dbe7184461ac8f9a
rohitjha424 <rohitjha424@gmail.com>
2026-02-03 23:49:50 +0530

    find_if() implemented

--- 9STL/2Algorithms/iteratorAlgorithms.cpp
+++ 9STL/2Algorithms/iteratorAlgorithms.cpp
@@ -3,28 +3,38 @@
 #include<vector>
 using namespace std;
 
-void printDouble(int a ){
-    cout<<2*a<<" ";
+// void printDouble(int a ){
+//     cout<<2*a<<" ";
+// }
+
+bool checkEven(int a){
+    return a%2 ==0;
 }
 
 
 int main(){
 
     vector<int> arr(5);
-    arr[0] = 10;   
+    arr[0] = 11;   
     arr[1] = 20;   
     arr[2] = 30;   
     arr[3] = 40;   
     arr[4] = 50;   
 
     //for_each()
-    for_each(arr.begin(), arr.end(), printDouble); // will take all the elements from begining to end and apply the printDouble() on it.
-    cout<<endl;
+    // for_each(arr.begin(), arr.end(), printDouble); // will take all the elements from begining to end and apply the printDouble() on it.
+    // cout<<endl;
 
-    //find()
-    int target = 40;
-    auto it = find(arr.begin(), arr.end(), target);
-    cout<<*it<<endl;  // will print the value of target if found and will print 0 if not found.
+    // //find()
+    // int target = 40;
+    // auto it = find(arr.begin(), arr.end(), target);
+    // cout<<*it<<endl;  // will print the value of target if found and will print 0 if not found.
+
+
+    //find_if()
+    auto it = find_if(arr.begin(), arr.end(), checkEven);
+    cout<<*it<<endl; 
+
 
     
 
