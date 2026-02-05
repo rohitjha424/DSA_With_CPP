f6d10ac97d5e0750bb189908d5c3668dcf646f31
rohitjha424 <rohitjha424@gmail.com>
2026-02-05 23:28:46 +0530

    accumulate() implemented

--- 9STL/2Algorithms/NumericAlgorithms.cpp
+++ 9STL/2Algorithms/NumericAlgorithms.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include <numeric>
 using namespace std;
 
 
@@ -11,7 +12,10 @@ int main(){
     arr[1] = 20;   
     arr[2] = 30;   
     arr[3] = 40;   
-    arr[4] = 50;    
+    arr[4] = 50;
+    
+    int totalSum = accumulate(arr.begin(), arr.end(), 0); // will sum from start to end each vector, sum initialized with 0
+    cout<<totalSum<<endl; // will print the sum of all elements in the vector
 
 
 
