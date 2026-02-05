46b4ffa3360452f6ce6dbfbd53fdbf1760a6bde6
rohitjha424 <rohitjha424@gmail.com>
2026-02-05 23:54:14 +0530

    partial_sum() implemented

--- 9STL/2Algorithms/NumericAlgorithms.cpp
+++ 9STL/2Algorithms/NumericAlgorithms.cpp
@@ -22,15 +22,25 @@ int main(){
     first.push_back(1);
     first.push_back(2);
     first.push_back(3);
+    first.push_back(4);
 
-    vector<int> second;
-    second.push_back(3);
-    second.push_back(4);
-    second.push_back(5);
+//     vector<int> second;
+//     second.push_back(3);
+//     second.push_back(4);
+//     second.push_back(5);
 
-   int inner_pro=  inner_product(first.begin(), first.end(), second.begin(), 0);
-   //inner product >> first[0]*second[0] + first[1]*second[1]+first[2]*second[2]
-   cout<<inner_pro<<endl;
+//    int inner_pro=  inner_product(first.begin(), first.end(), second.begin(), 0);
+//    //inner product >> first[0]*second[0] + first[1]*second[1]+first[2]*second[2]
+//    cout<<inner_pro<<endl;
+
+   //partial_sum()
+   vector<int> resultParcialSum(first.size());
+    partial_sum(first.begin(), first.end(), resultParcialSum.begin());
+    for(int a: resultParcialSum){ // as array is 1, 2, 3, 4 >> will print >>
+        cout<<a<<" ";  // 1>>1+2>>3, 1+2+3 >>6, 1+2+3+4>>10
+    }
+
+
 
 
 
