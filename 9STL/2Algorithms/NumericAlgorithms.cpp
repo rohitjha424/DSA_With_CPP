4326e6740079ef75b6ab7c7d213bed88a21b71a1
rohitjha424 <rohitjha424@gmail.com>
2026-02-05 23:21:16 +0530

    Numeric Algorithms Started

---
+++ 9STL/2Algorithms/NumericAlgorithms.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+int main(){
+
+    vector<int> arr(5);
+    arr[0] = 10;   
+    arr[1] = 20;   
+    arr[2] = 30;   
+    arr[3] = 40;   
+    arr[4] = 50;    
+
+
+
+
+
+
+
+
+    return 0;
+}
