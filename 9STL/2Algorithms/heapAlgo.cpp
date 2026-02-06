c69294eded5aec807ff6ec7411fcf404209cc8ac
rohitjha424 <rohitjha424@gmail.com>
2026-02-06 23:22:17 +0530

    push_heap() implemented to restore heap property

--- 9STL/2Algorithms/heapAlgo.cpp
+++ 9STL/2Algorithms/heapAlgo.cpp
@@ -21,7 +21,20 @@ int main()
     for(int a: arr){
         cout<<a<<" ";   // will print : 77 66 55 22 11 >> it bacame max_heap
     }cout<<endl;
-
+    
+    //insertion will break the heap property
+    // arr.push_back(99);
+    // for(int a: arr){
+    //     cout<<a<<" ";   // will print : 77 66 55 22 11 99 >> it broke heap property
+    // }cout<<endl;
+    
+    //>> we need to use the push_heap()>> to push without breaking the heap property
+    arr.push_back(99); // pushing a number
+    push_heap(arr.begin(), arr.end()); // will maintain order of heap 
+    for(int a: arr){
+        cout<<a<<" ";   // will print : 99 66 77 22 11 55 >> it broke heap property
+    }cout<<endl;
+    
 
     return 0;
 }
