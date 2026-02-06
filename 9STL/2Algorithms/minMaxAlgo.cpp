06ec60953311cd1f1b3b50a8af81114bb4de9ee9
rohitjha424 <rohitjha424@gmail.com>
2026-02-06 22:46:36 +0530

    min_element() implemented on vector

--- 9STL/2Algorithms/minMaxAlgo.cpp
+++ 9STL/2Algorithms/minMaxAlgo.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include<algorithm>
 // #include<algorithm>
 using namespace std;
 
@@ -6,15 +8,31 @@ int main()
 {
 
     //compare 2 numbers
-    int a = 10;
-    int b = 15;
+    // int a = 10;
+    // int b = 15;
 
-    //max() >> will print max of two numbers
-    cout<<max(a,b)<<endl; // will print 15
+    // //max() >> will print max of two numbers
+    // cout<<max(a,b)<<endl; // will print 15
 
         
-    //min() >> will print min of two numbers
-    cout<<min(a,b)<<endl; // will print 10
+    // //min() >> will print min of two numbers
+    // cout<<min(a,b)<<endl; // will print 10
+
+
+    //min/max in a range (container)
+
+    vector<int> arr;
+
+    // insertion of values in vector
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(40);
+    arr.push_back(50);
+
+    //min_element >> wll print minimum element from the container
+    auto it = min_element(arr.begin(), arr.end());
+    cout << *it<<endl;
 
 
 
