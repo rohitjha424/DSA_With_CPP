8d717a38fa544a8400f6f52e260dbf095167dcdb
rohitjha424 <rohitjha424@gmail.com>
2026-02-06 22:38:57 +0530

    max() implemented

---
+++ 9STL/2Algorithms/minMaxAlgo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include<algorithm>
+using namespace std;
+
+int main()
+{
+
+    //compare 2 numbers
+    int a = 10;
+    int b = 15;
+
+    //max() >> will print max of two numbers
+    cout<<max(a,b)<<endl; // will print 15
+
+
+
+    return 0;
+}
