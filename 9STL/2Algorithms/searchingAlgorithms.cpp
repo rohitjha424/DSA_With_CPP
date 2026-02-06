5fa194c6ccef01cafda42a545cd4b26a585efd58
rohitjha424 <rohitjha424@gmail.com>
2026-02-06 22:07:34 +0530

    binary_search() implemented

--- 9STL/2Algorithms/searchingAlgorithms.cpp
+++ 9STL/2Algorithms/searchingAlgorithms.cpp
@@ -6,6 +6,19 @@ using namespace std;
 
 int main(){
 
+    vector<int> arr;
+
+    // insertion of values in vector
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(40);
+    arr.push_back(50);
+
+    int target = 40;
+    auto it = binary_search(arr.begin(), arr.end(), target); // will return boolean if target found 
+    cout<<it<<endl; // will return 1
+
 
 return 0;
 }
