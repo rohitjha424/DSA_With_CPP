71c3afe6bf62b5e2ed5e5e57f9960bae56b5f840
rohitjha424 <rohitjha424@gmail.com>
2026-02-07 23:56:53 +0530

    forward iteration in bidresctional list

--- 9STL/Iterators/iteratorBasics.cpp
+++ 9STL/Iterators/iteratorBasics.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<vector>
 #include<forward_list>
+#include <list>
 using namespace std;
 
 int main(){
@@ -25,26 +26,45 @@ int main(){
 
     //forward_list iterator>> 
 
-    forward_list<int> fl;
-    fl.push_front(10);
-    fl.push_front(20);
-    fl.push_front(30);
-    fl.push_front(40);
+    // forward_list<int> fl;
+    // fl.push_front(10);
+    // fl.push_front(20);
+    // fl.push_front(30);
+    // fl.push_front(40);
 
-    forward_list<int>::iterator it = fl.begin();
+    // forward_list<int>::iterator it = fl.begin();
+    
+    // while(it != fl.end()){
+    //     (*it) = (*it) +5; // will add 5 to each element
+    //     it++;
+    // }
+    // cout<<endl;
 
-    while(it != fl.end()){
-        (*it) = (*it) +5; // will add 5 to each element
-        it++;
-    }
-    cout<<endl;
+    // it = fl.begin();
+    // while(it != fl.end()){
+    //     cout<<*it<<" ";
+    //     it++;
+    // }
+    // cout<<endl;
+    
+    //biderectional iterator
+    list<int> l;
 
-    it = fl.begin();
-    while(it != fl.end()){
+    // push back 
+    l.push_front(20);
+    l.push_front(10);
+    //push front
+    l.push_back(30);
+    l.push_back(40);
+    
+    //forward traversing 
+   list<int>::iterator it = l.begin();
+   while(it != l.end()){
         cout<<*it<<" ";
         it++;
-    }
-    cout<<endl;
+    }cout<<endl;
+
+   
 
 
     return 0;
