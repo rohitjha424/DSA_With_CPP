d582254a7617607c089bf268e7a5a9f690b694e0
rohitjha424 <rohitjha424@gmail.com>
2026-02-07 23:47:18 +0530

    forward_list iterator

--- 9STL/Iterators/iteratorBasics.cpp
+++ 9STL/Iterators/iteratorBasics.cpp
@@ -1,22 +1,39 @@
 //learning about iterator
 #include<iostream>
 #include<vector>
+#include<forward_list>
 using namespace std;
 
 int main(){
 
 
-    vector<int> arr;
-    arr.push_back(10);
-    arr.push_back(20);
-    arr.push_back(30);
+    // vector<int> arr;
+    // arr.push_back(10);
+    // arr.push_back(20);
+    // arr.push_back(30);
 
-    //traversing using iterator
+    // //traversing using iterator
 
-    //creating iterator
-    vector<int>::iterator it = arr.begin();
+    // //creating iterator
+    // vector<int>::iterator it = arr.begin();
 
-    while(it != arr.end()){
+    // while(it != arr.end()){
+    //     cout<<*it<<" ";
+    //     it++;
+    // }
+    // cout<<endl;
+
+    //forward_list iterator>> 
+
+    forward_list<int> fl;
+    fl.push_front(10);
+    fl.push_front(20);
+    fl.push_front(30);
+    fl.push_front(40);
+
+    forward_list<int>::iterator it = fl.begin();
+
+    while(it != fl.end()){
         cout<<*it<<" ";
         it++;
     }
