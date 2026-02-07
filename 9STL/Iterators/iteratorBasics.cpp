06ecc6065cff28ca7ff7572b3b1241cd38f38f41
rohitjha424 <rohitjha424@gmail.com>
2026-02-07 23:30:42 +0530

    traversing using iterator

--- 9STL/Iterators/iteratorBasics.cpp
+++ 9STL/Iterators/iteratorBasics.cpp
@@ -1,9 +1,26 @@
 //learning about iterator
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
 
+
+    vector<int> arr;
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+
+    //traversing using iterator
+
+    //creating iterator
+    vector<int>::iterator it = arr.begin();
+
+    while(it != arr.end()){
+        cout<<*it<<" ";
+        it++;
+    }
+    cout<<endl;
 
 
     return 0;
