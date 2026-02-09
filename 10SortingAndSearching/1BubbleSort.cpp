07bdff9d2a0a22c0c9b8145e7369ecca24cc0a70
rohitjha424 <rohitjha424@gmail.com>
2026-02-09 23:45:45 +0530

    bubble sort implemented

--- 10SortingAndSearching/1BubbleSort.cpp
+++ 10SortingAndSearching/1BubbleSort.cpp
@@ -1,54 +1,68 @@
-//Learning All About Bubble Sort
-#include<iostream>
-#include<vector>
+// Learning All About Bubble Sort
+// time complexity >> O(n square)
+
+#include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int size){
-    
-    for(int i =0; i<size-1; i++){
-        for(int j =0; j<size-i-1; j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
-            }
-        }
-    }
-}
-void bubbleSortDecOrder(int arr[], int size){
-    
-    for(int i =0; i<size-1; i++){
-        for(int j =0; j<size-i-1; j++){
-            if(arr[j]<arr[j+1]){ // will print in decreasing order
-                swap(arr[j],arr[j+1]);
-            }
-        }
-    }
-}
-int main(){
+void bubbleSort(int arr[], int size)
+{
 
-    int arr[] = {4,6,7,2,8} ;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+void bubbleSortDecOrder(int arr[], int size)
+{
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            { // will print in decreasing order
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int arr[] = {4, 6, 7, 2, 8};
     int size = 5;
 
-    cout<<"Before Sorting: "<<endl;
-    for(int a : arr){
-        cout<<a<<" ";
+    cout << "Before Sorting: " << endl;
+    for (int a : arr)
+    {
+        cout << a << " ";
     }
-    cout<<endl;
+    cout << endl;
 
     bubbleSort(arr, size);
 
-    cout<<"After Sorting: "<<endl;
-    for(int a : arr){
-        cout<<a<<" ";
+    cout << "After Sorting: " << endl;
+    for (int a : arr)
+    {
+        cout << a << " ";
     }
-    cout<<endl;
-   
+    cout << endl;
+
     bubbleSortDecOrder(arr, size);
 
-    cout<<"After Dec order Sorting: "<<endl;
-    for(int a : arr){
-        cout<<a<<" ";
+    cout << "After Dec order Sorting: " << endl;
+    for (int a : arr)
+    {
+        cout << a << " ";
     }
-    cout<<endl;
+    cout << endl;
 
     return 0;
 }
