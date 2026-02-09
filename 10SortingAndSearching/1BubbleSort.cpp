fa618bef7555395879fb56bce3ccde5c966ebf7e
rohitjha424 <rohitjha424@gmail.com>
2026-02-09 23:41:46 +0530

    Bubble sort in decreasing order implemented

--- 10SortingAndSearching/1BubbleSort.cpp
+++ 10SortingAndSearching/1BubbleSort.cpp
@@ -13,6 +13,16 @@ void bubbleSort(int arr[], int size){
         }
     }
 }
+void bubbleSortDecOrder(int arr[], int size){
+    
+    for(int i =0; i<size-1; i++){
+        for(int j =0; j<size-i-1; j++){
+            if(arr[j]<arr[j+1]){ // will print in decreasing order
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
 int main(){
 
     int arr[] = {4,6,7,2,8} ;
@@ -27,6 +37,14 @@ int main(){
     bubbleSort(arr, size);
 
     cout<<"After Sorting: "<<endl;
+    for(int a : arr){
+        cout<<a<<" ";
+    }
+    cout<<endl;
+   
+    bubbleSortDecOrder(arr, size);
+
+    cout<<"After Dec order Sorting: "<<endl;
     for(int a : arr){
         cout<<a<<" ";
     }
