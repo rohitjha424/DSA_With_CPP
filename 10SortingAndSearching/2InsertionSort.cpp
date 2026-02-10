eb569dfe605f2e79b43966c14d7f3055c1890251
rohitjha424 <rohitjha424@gmail.com>
2026-02-10 22:54:14 +0530

    insertion sorting done

--- 10SortingAndSearching/2InsertionSort.cpp
+++ 10SortingAndSearching/2InsertionSort.cpp
@@ -8,7 +8,17 @@ using namespace std;
 
 void insertionSort(vector<int> &arr)
 {
-  
+    int size = arr.size();
+    for (int i = 0; i < size; ++i)
+    {
+        int key = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
 }
 
 
@@ -24,6 +34,14 @@ int main()
     cout << endl;
 
     insertionSort(arr);
+
+    cout << "After Sorting: " << endl;
+    for (int a : arr)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+
 
 
     return 0;
