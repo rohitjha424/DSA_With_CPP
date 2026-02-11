251438635572d48c7c9c72696eb394e097155dd1
rohitjha424 <rohitjha424@gmail.com>
2026-02-11 23:59:52 +0530

    Selection Implement attempt

--- 10SortingAndSearching/3SelectionSort.cpp
+++ 10SortingAndSearching/3SelectionSort.cpp
@@ -2,18 +2,35 @@
 
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
 void selectionSort(vector<int> &arr)
 {
     int size = arr.size();
-   
+    //finding min element
+    
+    for (int i = 0; i < size-1; i++)
+    {
+       
+           int minIndex = i;
+        
+
+        for(int j =i+1; j<size; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+       
+        
+    }
+    cout<< "Min is "<<min<<endl;
 }
 
 int main()
 {
-    vector<int> arr = {5, 4, 3, 2, 1};
+    vector<int> arr = {5, 4, 2, 7, 9, 6};
 
     cout << "Before Sorting: " << endl;
     for (int a : arr)
