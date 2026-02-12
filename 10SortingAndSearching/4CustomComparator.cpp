031996513ed9c4714c004f90ddd49aa0bb3172a2
rohitjha424 <rohitjha424@gmail.com>
2026-02-13 00:03:26 +0530

    Comments Added

--- 10SortingAndSearching/4CustomComparator.cpp
+++ 10SortingAndSearching/4CustomComparator.cpp
@@ -3,22 +3,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 void print(vector<int> &arr){
     for(int i =0; i<arr.size(); i++){
         cout<<arr[i]<<" ";
     }cout<<endl;
-
-
 }
 
 bool myComp(int&a, int&b){
-    return a<b;
+    return a<b; // Ascending order  
 }
+
 bool myCompDec(int&a, int&b){
-    return a>b;
+    return a>b;  // Descending order  
 }
 
 int main(){
@@ -31,9 +29,14 @@ int main(){
     }
     cout << endl;
 
+    //custom Asceniding Order comparator
     sort(arr.begin(), arr.end(), myComp);
+    cout << "After custom Sorting in Ascending Order: " << endl;
     print(arr);
+
+    //custom Desceniding Order comparator
     sort(arr.begin(), arr.end(), myCompDec);
+    cout << "After custom Sorting in Descending Order: " << endl;
     print(arr);
     
     return 0;
