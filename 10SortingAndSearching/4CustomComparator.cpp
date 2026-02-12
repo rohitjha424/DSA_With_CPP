0ca5dc485406d423e74dee1d8623df7f2998f440
rohitjha424 <rohitjha424@gmail.com>
2026-02-12 23:59:21 +0530

    custom sorting for ascending order

--- 10SortingAndSearching/4CustomComparator.cpp
+++ 10SortingAndSearching/4CustomComparator.cpp
@@ -2,10 +2,34 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+void print(vector<int> &arr){
+    for(int i =0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+
+}
+
+bool myComp(int&a, int&b){
+    return a<b;
+}
+
 int main(){
+     vector<int> arr = {5, 4, 2, 7, 9, 6};
+
+    cout << "Before custom Sorting: " << endl;
+    for (int a : arr)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+
+    sort(arr.begin(), arr.end(), myComp);
+    print(arr);
     
     return 0;
 
