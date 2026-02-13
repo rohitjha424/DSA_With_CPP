749485df5f5198796f35c3632c01e725cfee7f3d
rohitjha424 <rohitjha424@gmail.com>
2026-02-14 00:10:39 +0530

    Code Optimized

--- 10SortingAndSearching/2Searching/1BinarySearchL1/2BinarySearchInDecOrder.cpp
+++ 10SortingAndSearching/2Searching/1BinarySearchL1/2BinarySearchInDecOrder.cpp
@@ -9,9 +9,9 @@ void binarySearchInDecOrder(vector<int> &arr, int target)
 {
     int start = 0;
     int end = arr.size() - 1;
-    int mid = start + (end-start)/2;
-    for (int i = 0; i < arr.size(); i++)
+    while(start<=end)
     {
+        int mid = start + (end-start)/2;
         if (arr[mid] == target)
         {
             cout << "Element found at index :" << mid << endl;
@@ -20,12 +20,10 @@ void binarySearchInDecOrder(vector<int> &arr, int target)
         else if (target < arr[mid])
         {
             start = mid + 1;
-            mid = (start + end) / 2;
         }
         else
         {
             end = mid - 1;
-            mid = (start + end) / 2;
         }
     }
 }
