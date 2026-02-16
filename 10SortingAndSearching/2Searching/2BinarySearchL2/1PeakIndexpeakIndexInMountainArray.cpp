d0f2bdb6b966a57c13074f3b19547a797251cafb
rohitjha424 <rohitjha424@gmail.com>
2026-02-16 23:06:02 +0530

    eak index in a mountain array Started

---
+++ 10SortingAndSearching/2Searching/2BinarySearchL2/1PeakIndexpeakIndexInMountainArray.cpp
@@ -0,0 +1,29 @@
+// peak index in a mountain array>> find peak element Index
+// Moutain Array is >> 0,2,5,3,1 >> first Increasing then Peak at 5 then decresing >>Maoutain
+// Ananlysis >> Max element is the Peak element>> find index of max
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void findPeakIndexMoutainArray(vector<int> &arr)
+{
+    int size = arr.size();
+    int start = 0;
+    int end = size - 1;
+    int peak = -1;
+    while (start <= end)
+    {
+   
+    }
+    cout << "Peak Element is: " << peak << endl;
+}
+
+int main()
+{
+    vector<int> arr = {2, 4, 6, 8, 5, 3, 1};
+
+    findPeakIndexMoutainArray(arr);
+
+    return 0;
+}
