fb88e0491404537bf17d8519e6bf548b42af2868
rohitjha424 <rohitjha424@gmail.com>
2026-02-16 23:59:41 +0530

    attempted

--- 10SortingAndSearching/2Searching/2BinarySearchL2/2FindSquareRoot.cpp
+++ 10SortingAndSearching/2Searching/2BinarySearchL2/2FindSquareRoot.cpp
@@ -3,10 +3,33 @@
 #include <iostream>
 using namespace std;
 
+int checkSqaure(int x){
+    int result = x*x ;
+        return result;
+}
 
 void findSquareRoot(int num)
 {
-    
+
+    int start = 0;
+    int end = num;
+     while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        
+        if(checkSqaure(mid) == num){
+            cout<< mid;
+            break;
+        }
+        if(checkSqaure(mid) > num){
+            end = mid -1;
+        }
+        else{
+            start = mid +1;
+        }
+
+    }
+
 }
 
 int main()
