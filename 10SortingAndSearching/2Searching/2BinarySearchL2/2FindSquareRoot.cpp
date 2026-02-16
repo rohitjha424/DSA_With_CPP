aaf84e0f6a50ee1f37de50d57aa4663782ba7482
rohitjha424 <rohitjha424@gmail.com>
2026-02-16 23:46:33 +0530

    Sqaure Root using Binary Search

---
+++ 10SortingAndSearching/2Searching/2BinarySearchL2/2FindSquareRoot.cpp
@@ -0,0 +1,22 @@
+// Find Square root of a number >> return only integer part if decimal
+
+#include <iostream>
+using namespace std;
+
+
+void findSquareRoot(int num)
+{
+    
+}
+
+int main()
+{
+    int num;
+   cout<<"Enter a Number:"<<endl;
+   cin>>num;
+
+
+    findSquareRoot(num);
+
+    return 0;
+}
