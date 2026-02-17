03d452d4b173d547dbe94356b94a3b03a9fc16c0
rohitjha424 <rohitjha424@gmail.com>
2026-02-17 23:32:44 +0530

    Char Array input / Output Operations

--- 11Strings/1StringsL1/1charArray.cpp
+++ 11Strings/1StringsL1/1charArray.cpp
@@ -5,5 +5,11 @@ using namespace std;
 
 int main(){
 
+    char arr[10];
+    //character Array input
+    cin>>arr;
+    //character Array Output in one line :
+    cout <<"your Inout is: "<<arr<<endl;
+
     return 0;
 }
