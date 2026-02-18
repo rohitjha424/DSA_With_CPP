ab565867f9ff1ed3cf2fe65ee061ea7a2f1da254
rohitjha424 <rohitjha424@gmail.com>
2026-02-18 23:57:43 +0530

    implementing getline()

--- 11Strings/1StringsL1/1charArray.cpp
+++ 11Strings/1StringsL1/1charArray.cpp
@@ -23,7 +23,13 @@ int main(){
     // cout<<arr2; // will print only :  rohit 
 
     //initialization
-    char arr[10] = "love"; // multiple character with double quotes
+    // char arr[10] = "love"; // multiple character with double quotes
+
+
+    // storing sequences of words with spaces using getline()
+    char arr1[50];
+    cin.getline(arr1,50);
+    cout<<arr1;
 
     return 0;
 }
